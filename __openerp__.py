4a21eb5f477cee96eada33a0695e1f445afe0e13
Eneldo Serrata <eneldoserrata@gmail.com>
2015-11-16 10:28:15 -0400

    add security groups

--- __openerp__.py
+++ __openerp__.py
@@ -25,7 +25,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/partner_view.xml',
         'views/shop_view.xml',
         'views/account_view.xml',
---
+++ doc/index.rst
@@ -0,0 +1,2 @@
+Manejador de comprobantes fiscales
+==================================
