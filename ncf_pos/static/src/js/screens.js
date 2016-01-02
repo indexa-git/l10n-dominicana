49ccfd1345333e467dbcc12380ab28fbab5b401d
Eneldo Serrata <eneldoserrata@gmail.com>
2016-01-01 22:44:20 -0400

    Add new modules

--- ncf_pos/static/src/js/screens.js
+++ ncf_pos/static/src/js/screens.js
@@ -4,11 +4,11 @@
 odoo.define('ncf_pos', function (require) {
     var screens = require('point_of_sale.screens');
 
-    screens.PaymentScreenWidget.include({
-        validate_order: function (force_validation) {
-            alert("ok");
-        }
-    })
+    //screens.PaymentScreenWidget.include({
+    //    validate_order: function (force_validation) {
+    //        alert("ok");
+    //    }
+    //})
 
 
 });
