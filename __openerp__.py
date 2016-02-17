037b54a816a3324d269d2de4126aba746610d976
Eneldo Serrata <eneldoserrata@gmail.com>
2016-02-17 11:49:45 -0400

    Add license info to all private modules

--- __openerp__.py
+++ __openerp__.py
@@ -1,29 +1,62 @@
 # -*- coding: utf-8 -*-
+
+########################################################################################################################
+#  Copyright (c) 2015 - Marcos Organizador de Negocios SRL. (<https://marcos.do/>) #  Write by Eneldo Serrata (eneldo@marcos.do)
+#  See LICENSE file for full copyright and licensing details.
+#
+# Odoo Proprietary License v1.0
+#
+# This software and associated files (the "Software") may only be used
+# (nobody can redistribute (or sell) your module once they have bought it, unless you gave them your consent)
+# if you have purchased a valid license
+# from the authors, typically via Odoo Apps, or if you have received a written
+# agreement from the authors of the Software (see the COPYRIGHT file).
+#
+# You may develop Odoo modules that use the Software as a library (typically
+# by depending on it, importing it and using its resources), but without copying
+# any source code or material from the Software. You may distribute those
+# modules under the license of your choice, provided that this license is
+# compatible with the terms of the Odoo Proprietary License (For example:
+# LGPL, MIT, or proprietary licenses similar to this one).
+#
+# It is forbidden to publish, distribute, sublicense, or sell copies of the Software
+# or modified copies of the Software.
+#
+# The above copyright notice and this permission notice must be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+########################################################################################################################
+
+
 {
     'name': "Comprobantes fiscales RD",
 
     'summary': """
-        Permite administrar y configurar comprobantes fiscales ademas de generara los reportes
-        606,607,608 y 609
+        Localizacion Para Republica Dominicana
+        Implementacin de numeros de comprobantes fiscales
         """,
 
     'description': """
-        Long description of module's purpose
+        Permite administrar y configurar comprobantes fiscales ademas de generara los reportes
+        606,607,608 y 609
     """,
 
-    'author': 'Eneldo Serrata - Marcos Organizador de Negocios, SRL.',
+    'author': "Marcos Organizador de Negocios SRL - Write by Eneldo Serrata",
     'website': "http://marcos.do",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Localization',
     'version': '1.0',
 
-    # any module necessary for this one to work correctly
+
     'depends': ['base','account', 'account_accountant', 'l10n_do','rnc_validation'],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'data/setup_ncf.xml',
@@ -42,6 +75,8 @@
     "qweb": ['static/src/xml/base.xml'],
     'demo': [],
     'images': 'static/description/main.png',
-    "price": 1500,
-    'currency': 'EUR'
+    "price": 2000,
+    'currency': 'EUR',
+    'license': "Other proprietary"
+
 }
