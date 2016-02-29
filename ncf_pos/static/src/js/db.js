17cf9b318906fda35205452080c035a99a228356
Eneldo Serrata <eneldoserrata@gmail.com>
2016-02-29 00:14:19 -0400

    rollback intention to select multible account template

---
+++ ncf_pos/static/src/js/db.js
@@ -0,0 +1,33 @@
+
+odoo.define('ncf_pos.DB', function (require) {
+    "use strict";
+
+    var db = require('point_of_sale.DB');
+
+    db.include({
+        _partner_search_string: function (partner) {
+            var str = partner.name;
+            if (partner.vat) {
+                str += '|' + partner.vat;
+            }
+            if (partner.barcode) {
+                str += '|' + partner.barcode;
+            }
+            if (partner.address) {
+                str += '|' + partner.address;
+            }
+            if (partner.phone) {
+                str += '|' + partner.phone.split(' ').join('');
+            }
+            if (partner.mobile) {
+                str += '|' + partner.mobile.split(' ').join('');
+            }
+            if (partner.email) {
+                str += '|' + partner.email;
+            }
+            str = '' + partner.id + ':' + str.replace(':', '') + '\n';
+            return str;
+        }
+    });
+
+});
