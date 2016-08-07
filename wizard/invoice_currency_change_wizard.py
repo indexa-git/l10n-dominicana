055f7dcc7acdd1e6691e7e6dc41e5089cd9e9e66
Eneldo Serrata <eneldoserrata@gmail.com>
2016-08-07 10:58:24 -0400

    Facturas doble moneda segunda moneda se muesrta siempre en USD Indicar cuáles son las variables a utilizar en la descripción de los términos de pago de la factura, a fin de indicar: tasa de cambio, monto de ITBIS de la factura, monto total de la factura, número de factura, fecha de factura, nombre de cliente. Mover "Imprimir dos monedas" a Términos de pago

--- wizard/invoice_currency_change_wizard.py
+++ wizard/invoice_currency_change_wizard.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models, fields, api, exceptions
 
 
 class InvoiceCurrecyChangeWizard(models.TransientModel):
@@ -14,8 +14,25 @@ class InvoiceCurrecyChangeWizard(models.TransientModel):
 
     currency_id = fields.Many2one("res.currency", domain=_get_currency_domain, require=True, string="Moneda")
 
+    def _get_rate(self, rate_date):
+        if self.currency_id:
+
+            self._cr.execute("""SELECT rate FROM res_currency_rate
+                               WHERE currency_id = %s
+                                 AND name = %s
+                                 AND (company_id is null
+                                     OR company_id = %s)
+                            ORDER BY company_id, name desc LIMIT 1""",
+                           (self.currency_id.id, rate_date, self.env.user.company_id.id))
+            if self._cr.rowcount > 0:
+                return (1 / self._cr.fetchone()[0])
+
+            return False
+
+
     @api.multi
     def update_invoice_currency(self):
+
         active_id = self._context.get("active_id")
         model = self._context.get("active_model")
         inv = self.env[model].browse(active_id)
@@ -24,10 +41,15 @@ class InvoiceCurrecyChangeWizard(models.TransientModel):
             if self.currency_id.id == self.env.user.company_id.currency_id.id:
                 for line in inv.invoice_line_ids:
                     line.price_unit = line.price_unit*inv.rate
-                inv.currency_id = self.currency_id.id
             else:
-                inv.currency_id = self.currency_id.id
+                new_rate = self._get_rate(inv.date_invoice)
+                if not new_rate:
+                    raise exceptions.ValidationError(u"Antes de cambiar la mondeda de la factura debe actulizar la tasa.")
                 for line in inv.invoice_line_ids:
-                   line.price_unit = line.price_unit/inv.rate
+                   line.price_unit = line.price_unit/new_rate
+
+            inv.currency_id = self.currency_id.id
+            inv.compute_taxes()
+            inv._compute_amount()
 
         return True
