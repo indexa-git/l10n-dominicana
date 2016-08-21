9867f1d493d7f3fd6f08209f5358b558878fed07
Eneldo Serrata <eneldoserrata@gmail.com>
2016-08-21 12:15:06 -0400

    IMP

--- ncf_pos/wizard/cash_box_control.py
+++ ncf_pos/wizard/cash_box_control.py
@@ -3,12 +3,12 @@
 from openerp import models, api, fields
 
 
-# class CheckCashBox(models.TransientModel):
-#     _name = "check.cash.box"
-#
-#
-#     dummy = fields.Boolean()
-#
-#     @api.model
-#     def confirm_cash_diff(self):
-#         import pdb;pdb.set_trace()
+class CheckCashBox(models.TransientModel):
+    _name = "check.cash.box"
+
+
+    dummy = fields.Boolean()
+
+    @api.model
+    def confirm_cash_diff(self):
+        import pdb;pdb.set_trace()
