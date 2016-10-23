d64979cc452f2fdca05e1fe485405d034a3effbb
eneldoserrata <eneldoserrata@gmail.com>
2016-10-22 21:33:49 -0400

    fix pos duplicate call to get_fiscal_info on POS when IPF are installed
    Former-commit-id: 05761254d3d6416bdac59ed23d8d6cb4c3d11adf

--- ncf_pos/static/src/js/gui.js
+++ ncf_pos/static/src/js/gui.js
@@ -9,16 +9,21 @@ odoo.define('ncf_pos.gui', function (require) {
             this.show_screen("ncf", undefined, undefined)
         },
         show_screen: function (screen_name, params, refresh) {
+            var self = this;
+            // console.log(self.pos.config.use_proxy);
+
 
             if(screen_name == "ncf") {
                 this._super("receipt", params, refresh);
             }
-            else if (screen_name === "receipt") {
-                var self = this;
+            else if (screen_name === "receipt" && self.pos.config.use_proxy != true) {
+
                 var order = this.pos.get_order();
+
                 new Model('pos.order').call("get_fiscal_data", [order.name]).then(function (result) {
                     order.set_ncf(result.ncf);
                     order.set_fiscal_type_name(result.fiscal_type_name);
+                    console.log("FROM NCF_POS")
                     console.log("get_fiscal_data");
                 }).done(function () {
                     self.show_screen_ncf()
