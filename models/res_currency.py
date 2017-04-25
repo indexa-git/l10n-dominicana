858ba9da33f9691900d23c6d9c7b43618f0c8cc5
eneldoserrata <eneldoserrata@gmail.com>
2017-04-25 13:15:23 -0400

    First commit of import curreny history from BC

---
+++ models/res_currency.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import re
+
+
+from odoo import api, fields, models, tools, _
+
+CURRENCY_DISPLAY_PATTERN = re.compile(r'(\w+)\s*(?:\((.*)\))?')
+
+
+class CurrencyRate(models.Model):
+    _inherit = "res.currency.rate"
+
+    @api.multi
+    def import_excel(self):
+        return {}
+
+
+class BcRateImport(models.TransientModel):
+    _name = "bc.rate.import"
+
+    excel_file = fields.Binary()
+
+
+    @api.multi
+    def import_excel(self):
+        pass
