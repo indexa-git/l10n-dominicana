256f932b75728303313dda4f5a5233fa3c568680
gustavovalverde <g.valverde02@gmail.com>
2017-06-11 14:49:28 -0400

    [DEL] Remove all uneeded and useless files

--- __manifest__.py
+++ __manifest__.py
@@ -27,24 +27,17 @@
     'data': [
         'data/data.xml',
         'security/ir.model.access.csv',
-        'security/ncf_manager_security.xml',
         'wizard/account_invoice_cancel_view.xml',
         'wizard/update_rate_wizard_view.xml',
         'wizard/account_invoice_refund.xml',
         'views/shop_view.xml',
         'views/account_invoice_view.xml',
         'views/account_view.xml',
-        'views/views.xml',
-        'views/templates.xml',
         'views/dgii_exterior_view.xml',
         'views/dgii_purchase_view.xml',
         'views/dgii_sale_view.xml',
         'views/dgii_cancel_view.xml',
         'views/res_view.xml',
         'data/setup_ncf.xml'
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
