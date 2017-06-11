d0a483f06d88096624730359b2932021ab647e47
gustavovalverde <g.valverde02@gmail.com>
2017-06-11 14:21:17 -0400

    [LNT] Make it PEP8 Compliant
    Former-commit-id: 6c098f86e9a449f09e139fa4c789ebeb63b04e10

--- ncf_pos/models/res_partner.py
+++ ncf_pos/models/res_partner.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-
 
     @api.model
     def create_from_ui(self, partner):
