4fac9f9efd78547be9ef271a3cf6a9c286fac6fd
Gustavo Valverde <g.valverde02@gmail.com>
2017-07-02 13:06:43 -0400

    [REF] Improve NCF Handling & Remove Webkul Logic (#1)
    NCF_POS had a great dependency on webkul modules, with this
    refactoring webkul functionalities like: show all orders, reprint and
    order returns are moved to their respective external modules.
    
    The idea behind this segregation is to make maintenance easier
    for pos localization.
    
    It also improves the NCF handling and makes a lot of minor fixes.
    
    * [FIX] Add missing comma
    
    * [FIX] Remove link to unexisting css file
    
    * [FIX] Add missing variable user_ids in pos_config
    
    * [FIX] Now we're using the partners for sequencing
    
    * [IMP] Add exceptions handling and force clients
    
    * [FIX] Add modification on clinets for localization
    
    * [FIX] remove duplicate information on pos_orders
    
    * [FIX] Make partner sale_fiscal_type editable
    
    * [FIX] Handle a case with a Factura in "None"
    
    If the order client has the field "Factura" in "None", because
    if was never assigned, then this client will get a Pre-Cuenta until
    it's changed back to normal
    
    * [IMP] Merge return message in PDF receipt
    
    * [REF][IMP] Move code & add note printing on orderline

--- ncf_pos/models/pos_order.py
+++ ncf_pos/models/pos_order.py
@@ -39,10 +39,7 @@
 import logging
 import time
 
-import psycopg2
-
-from odoo import api, exceptions, fields, models, tools, _
-from odoo.tools import float_is_zero
+from odoo import api, exceptions, fields, models, _
 
 from odoo.tools.safe_eval import safe_eval
 
@@ -60,126 +57,10 @@ class PosOrder(models.Model):
         IrConfigParam = self.env['ir.config_parameter']
         return safe_eval(IrConfigParam.get_param('ncf_pos.pos_session_picking_on_cron', 'False'))
 
-    is_return_order = fields.Boolean(string='Devolver orden', copy=False)
-    return_order_id = fields.Many2one('pos.order', u'Orden de devolución de',
-                                      readonly=True, copy=False)
-
-    return_status = fields.Selection(
-        [('-', 'Sin Devoluciones'),
-         ('Fully-Returned', 'Totalmente devuelto'),
-         ('Partially-Returned', 'Devuelto parcialmente'),
-         ('Non-Returnable', 'No retornable')],
-        default='-', copy=False, string=u'Estado de devolución')
-
     move_name = fields.Char(size=19)
     fiscal_nif = fields.Char()
     invoice_number = fields.Char(related="invoice_id.number")
     is_service_order = fields.Boolean("Ordenes que no generan picking")
-
-    @api.model
-    def create_from_ui(self, orders):
-        for order in orders:
-            order.update({"to_invoice": True})
-
-        submitted_references = [o['data']['name'] for o in orders]
-        pos_order = self.search([('pos_reference', 'in', submitted_references)])
-        existing_orders = pos_order.read(['pos_reference'])
-        existing_references = set([o['pos_reference'] for o in existing_orders])
-        orders_to_save = [o for o in orders if o['data']['name'] not in existing_references]
-        order_ids = []
-
-        for tmp_order in orders_to_save:
-            to_invoice = tmp_order['to_invoice']
-            order = tmp_order['data']
-            if to_invoice:
-                self._match_payment_to_invoice(order)
-            pos_order = self._process_order(order)
-            order_ids.append(pos_order.id)
-
-            try:
-                pos_order.action_pos_order_paid()
-            except psycopg2.OperationalError:
-                # do not hide transactional errors, the order(s) won't be saved!
-                raise
-            except Exception as e:
-                _logger.error('Could not fully process the POS Order: %s', tools.ustr(e))
-
-        order_objs = self.env['pos.order'].browse(order_ids)
-        result = {}
-        order_list = []
-        order_line_list = []
-        statement_list = []
-        for order_obj in order_objs:
-            vals = {}
-            vals['lines'] = []
-            if hasattr(order_objs[0], 'return_status'):
-                if not order_obj.is_return_order:
-                    vals['return_status'] = order_obj.return_status
-                    vals['existing'] = False
-                    vals['id'] = order_obj.id
-                else:
-                    order_obj.return_order_id.return_status = order_obj.return_status
-                    vals['existing'] = True
-                    vals['id'] = order_obj.id
-                    vals['original_order_id'] = order_obj.return_order_id.id
-                    vals['return_status'] = order_obj.return_order_id.return_status
-                    for line in order_obj.lines:
-                        line_vals = {}
-                        line_vals['id'] = line.original_line_id.id
-                        line_vals['line_qty_returned'] = line.original_line_id.line_qty_returned
-                        line_vals['existing'] = True
-                        order_line_list.append(line_vals)
-            vals['statement_ids'] = order_obj.statement_ids.ids
-            vals['name'] = order_obj.name
-            vals['amount_total'] = order_obj.amount_total
-            vals['pos_reference'] = order_obj.pos_reference
-            vals['date_order'] = order_obj.date_order
-            if order_obj.invoice_id:
-                vals['invoice_id'] = order_obj.invoice_id.id
-            else:
-                vals['invoice_id'] = False
-            if order_obj.partner_id:
-                vals['partner_id'] = [order_obj.partner_id.id, order_obj.partner_id.name]
-            else:
-                vals['partner_id'] = False
-            if (not hasattr(order_objs[0], 'return_status') or (
-                        hasattr(order_objs[0], 'return_status') and not order_obj.is_return_order)):
-                vals['id'] = order_obj.id
-                for line in order_obj.lines:
-                    vals['lines'].append(line.id)
-                    line_vals = {}
-                    # LINE DATAA
-                    line_vals['create_date'] = line.create_date
-                    line_vals['discount'] = line.discount
-                    line_vals['display_name'] = line.display_name
-                    line_vals['id'] = line.id
-                    line_vals['order_id'] = [line.order_id.id, line.order_id.name]
-                    line_vals['price_subtotal'] = line.price_subtotal
-                    line_vals['price_subtotal_incl'] = line.price_subtotal_incl
-                    line_vals['price_unit'] = line.price_unit
-                    line_vals['product_id'] = [line.product_id.id, line.product_id.name]
-                    line_vals['qty'] = line.qty
-                    line_vals['write_date'] = line.write_date
-                    if hasattr(line, 'line_qty_returned'):
-                        line_vals['line_qty_returned'] = line.line_qty_returned
-                    # LINE DATAA
-                    order_line_list.append(line_vals)
-                for statement_id in order_obj.statement_ids:
-                    statement_vals = {}
-                    # STATEMENT DATAA
-                    statement_vals['amount'] = statement_id.amount
-                    statement_vals['id'] = statement_id.id
-                    if statement_id.journal_id:
-                        currency = statement_id.journal_id.currency_id or statement_id.journal_id.company_id.currency_id
-                        statement_vals['journal_id'] = [statement_id.journal_id.id, statement_id.journal_id.name + " (" + currency.name + ")"]
-                    else:
-                        statement_vals['journal_id'] = False
-                    statement_list.append(statement_vals)
-            order_list.append(vals)
-        result['orders'] = order_list
-        result['orderlines'] = order_line_list
-        result['statements'] = statement_list
-        return result
 
     @api.multi
     def action_pos_order_paid(self):
@@ -204,8 +85,8 @@ class PosOrder(models.Model):
 
     @api.model
     def get_fiscal_data(self, name):
-        res = {"fiscal_type": "none", "fiscal_type_name": u"CUENTA"}
-
+        res = {"fiscal_type": "none", "fiscal_type_name": u"PRE-CUENTA"}
+        sequence = False
         timeout = time.time() + 60 * 0.5
         while True:
             time.sleep(1)
@@ -213,15 +94,13 @@ class PosOrder(models.Model):
             self._cr.commit()
             if order_id or time.time() > timeout:
                 break
+
         if order_id:
             shop_user_config = self.env["shop.ncf.config"].get_user_shop_config()
-
             res.update({"id": order_id.id,
                         "rnc": order_id.partner_id.vat,
                         "name": order_id.partner_id.name})
-        #     res.update({"ncf": order_id.invoice_id.number,
-        #                 "id": order_id.id, "rnc": order_id.partner_id.vat,
-        #                 "name": order_id.partner_id.name})
+
             if order_id.is_return_order:
                 res.update({"fiscal_type_name": u"NOTA DE CRÉDITO"})
 
@@ -230,7 +109,6 @@ class PosOrder(models.Model):
 
                 if reference_ncf_type in ("01", "14"):
                     res.update({"fiscal_type": "fiscal_note"})
-
                 elif reference_ncf_type == "02":
                     res.update({"fiscal_type": "final_note"})
                 elif reference_ncf_type == "15":
@@ -238,108 +116,35 @@ class PosOrder(models.Model):
                 res.update({"origin": reference_ncf})
                 sequence = shop_user_config.nc_sequence_id
 
-            elif order_id.invoice_id.sale_fiscal_type == "fiscal":
+            elif order_id.partner_id.sale_fiscal_type == "fiscal":
                 res.update({"fiscal_type": "fiscal",
                             "fiscal_type_name": "FACTURA CON VALOR FISCAL",
                             "origin": False})
                 sequence = shop_user_config.fiscal_sequence_id
 
-            elif order_id.invoice_id.sale_fiscal_type == "final":
+            elif order_id.partner_id.sale_fiscal_type == "final":
                 res.update({"fiscal_type": "final",
                            "fiscal_type_name": "FACTURA PARA CONSUMIDOR FINAL",
                             "origin": False})
                 sequence = shop_user_config.final_sequence_id
 
-            elif order_id.invoice_id.sale_fiscal_type == "gov":
+            elif order_id.partner_id.sale_fiscal_type == "gov":
                 res.update({"fiscal_type": "fiscal",
                             "fiscal_type_name": "FACTURA GUBERNAMENTAL",
                             "origin": False})
                 sequence = shop_user_config.gov_sequence_id
 
-            elif order_id.invoice_id.sale_fiscal_type == "special":
+            elif order_id.partner_id.sale_fiscal_type == "special":
                 res.update({"fiscal_type": "special",
                            "fiscal_type_name": u"FACTURA PARA REGÍMENES ESPECIALES",
                             "origin": False})
                 sequence = shop_user_config.special_sequence_id
-
-            order_id.move_name = sequence.with_context(ir_sequence_date=fields.Date.today()).next_by_id()
-            res.update({"ncf": order_id.move_name})
+            if sequence:
+                order_id.move_name = sequence.with_context(ir_sequence_date=fields.Date.today()).next_by_id()
+                res.update({"ncf": order_id.move_name})
             order_id.action_pos_order_invoice()
-            res.update({"ncf": order_id.move_name})
 
         return res
-
-    @api.model
-    def _order_fields(self, ui_order):
-        fields_return = super(PosOrder, self)._order_fields(ui_order)
-        fields_return.update({
-            'is_return_order': ui_order.get('is_return_order') or False,
-            'return_order_id': ui_order.get('return_order_id') or False,
-            'return_status': ui_order.get('return_status') or False,
-        })
-        return fields_return
-
-    @api.model
-    def _process_order(self, pos_order):
-        prec_acc = self.env['decimal.precision'].precision_get('Account')
-        pos_session = self.env['pos.session'].browse(pos_order['pos_session_id'])
-        if pos_session.state == 'closing_control' or pos_session.state == 'closed':
-            pos_order['pos_session_id'] = self._get_valid_session(pos_order).id
-        if pos_order['is_return_order']:
-            pos_order['amount_paid'] = 0
-            for line in pos_order['lines']:
-                line_dict = line[2]
-                line_dict['qty'] = line_dict['qty'] * -1
-                original_line = self.env['pos.order.line'].browse(line_dict['original_line_id'])
-                original_line.line_qty_returned += abs(line_dict['qty'])
-            for statement in pos_order['statement_ids']:
-                statement_dict = statement[2]
-                statement_dict['amount'] = statement_dict['amount'] * -1
-            pos_order['amount_tax'] = pos_order['amount_tax'] * -1
-            pos_order['amount_return'] = 0
-            pos_order['amount_total'] = pos_order['amount_total'] * -1
-
-        order = self.create(self._order_fields(pos_order))
-        journal_ids = set()
-        for payments in pos_order['statement_ids']:
-            if not float_is_zero(payments[2]['amount'],
-                                 precision_digits=prec_acc):
-                order.add_payment(self._payment_fields(payments[2]))
-            journal_ids.add(payments[2]['journal_id'])
-
-        if pos_session.sequence_number <= pos_order['sequence_number']:
-            pos_session.write(
-                        {'sequence_number': pos_order['sequence_number'] + 1})
-            pos_session.refresh()
-
-        if not float_is_zero(pos_order['amount_return'], prec_acc):
-            cash_journal_id = pos_session.cash_journal_id.id
-            if not cash_journal_id:
-                # Select for change one of the cash journals used in this
-                # payment
-                cash_journal = self.env['account.journal'].search([
-                    ('type', '=', 'cash'),
-                    ('id', 'in', list(journal_ids))],
-                    limit=1)
-                if not cash_journal:
-                    # If none, select for change one of the cash journals
-                    # of the POS
-                    # This is used for example when a customer pays by
-                    # credit card an amount higher than total amount of the
-                    # order and gets cash back
-                    cash_journal = [statement.journal_id for statement in pos_session.statement_ids if
-                                    statement.journal_id.type == 'cash']
-                    if not cash_journal:
-                        raise exceptions.UserError(_(
-                            u"No se encontró ninguna declaración de efectivo para esta sesión. No se puede registrar el efectivo devuelto."))
-                cash_journal_id = cash_journal[0].id
-            order.add_payment({
-                'amount': -pos_order['amount_return'],
-                'payment_date': fields.Datetime.now(),
-                'payment_name': _('return'),
-                'journal': cash_journal_id,
-            })
-        return order
 
     @api.multi
     def action_pos_order_invoice(self):
@@ -389,7 +194,7 @@ class PosOrder(models.Model):
                     limit=100)
         print invoice_ids
         print invoice_ids.ids
-        order_ids = self.search([('invoice_id','in',invoice_ids.ids)])
+        order_ids = self.search([('invoice_id', 'in', invoice_ids.ids)])
         print order_ids
 
         order_list = []
@@ -424,17 +229,3 @@ class PosOrder(models.Model):
                 }
                 order_lines_list.append(order_lines_json)
         return {"wk_order": order_list, "wk_order_lines": order_lines_list}
-
-
-class PosOrderLine(models.Model):
-    _inherit = 'pos.order.line'
-
-    line_qty_returned = fields.Integer(u'Línea devuelta', default=0)
-    original_line_id = fields.Many2one('pos.order.line', u"Línea original")
-
-    @api.model
-    def _order_line_fields(self, line):
-        fields_return = super(PosOrderLine, self)._order_line_fields(line)
-        fields_return[2].update({'line_qty_returned': line[2].get('line_qty_returned', '')})
-        fields_return[2].update({'original_line_id': line[2].get('original_line_id', '')})
-        return fields_return
