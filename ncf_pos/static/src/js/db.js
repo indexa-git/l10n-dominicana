0070fb3a6f045fed0220814cfceb73243dad3172
gustavovalverde <g.valverde02@gmail.com>
2017-07-22 05:51:04 -0400

    Merge branch '10.0' of github.com:eneldoserrata/marcos_addons into eneldoserrata-10.0
    Former-commit-id: 3bb30ae1ea6504a894d2dc20d97e02256ba683fc

---
+++ ncf_pos/static/src/js/db.js
@@ -0,0 +1,15 @@
+odoo.define('ncf_pos.db', function (require) {
+    "use strict";
+    var pos_db = require('point_of_sale.DB');
+
+    pos_db.include({
+        _partner_search_string: function (partner) {
+            var str = this._super(partner);
+            str = str.replace('\n', '');
+            if (partner.vat) {
+                str += '|' + partner.vat;
+            }
+            return str + '\n';
+        }
+    });
+});
