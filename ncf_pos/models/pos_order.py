c76c5605a35d819467d803531ace600dae220290
gustavovalverde <g.valverde02@gmail.com>
2017-09-19 17:50:12 -0400

    [FIX] My blindness is OP

--- ncf_pos/models/pos_order.py
+++ ncf_pos/models/pos_order.py
@@ -57,8 +57,8 @@ class PosOrder(models.Model):
         res = super(PosOrder, self)._prepare_invoice()
         if self.is_return_order:
             res.update({"type": "out_refund",
-                        "origin_invoice_ids": [(4, self.return_order_id.invoice_id.id, _),
-                        "origin": self.return_order_id.move_name]})
+                        "origin_invoice_ids": [(4, self.return_order_id.invoice_id.id, _)],
+                        "origin": self.return_order_id.move_name})
         res.update({"move_name": self.move_name})
         if self.fiscal_nif:
             res.update({"fiscal_nif": self.fiscal_nif})
