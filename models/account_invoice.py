d3b954fbca853b8552d12663fec61f87a0cf9752
gustavovalverde <g.valverde02@gmail.com>
2017-12-17 16:58:46 -0400

    [REF] 1st invoice_refund changes for optimization

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -230,3 +230,15 @@ class AccountInvoice(models.Model):
                 self.env["marcos.api.tools"].invoice_ncf_validation(rec)
 
         return super(AccountInvoice, self).action_invoice_open()
+
+    @api.model
+    def _prepare_refund(self, invoice, date_invoice=None, date=None,
+                        description=None, journal_id=None):
+        res = super(AccountInvoice, self)._prepare_refund(
+            invoice, date_invoice=date_invoice, date=date,
+            description=description, journal_id=journal_id)
+
+        if self._context.get("credit_note_supplier_ncf", False):
+            res.update({"move_name": self._context["credit_note_supplier_ncf"]
+                        })
+        return res
