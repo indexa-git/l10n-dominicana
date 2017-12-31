115042da74fe2c3f88ca0259fab9f3fcd86f4f6b
gustavovalverde <g.valverde02@gmail.com>
2017-12-31 12:53:12 -0400

    [TEST] Add create_from_ui and remove action_pos_order_invoice

--- pos_order_return/models/pos_order_return.py
+++ pos_order_return/models/pos_order_return.py
@@ -95,72 +95,72 @@ class PosOrder(models.Model):
             })
         return order
 
-    @api.multi
-    def action_pos_order_invoice(self):
-        Invoice = self.env['account.invoice']
-        for order in self:
-            if not order.is_return_order:
-                super(PosOrder, order).action_pos_order_invoice()
-            else:
-                local_context = dict(
-                    self.env.context, force_company=order.company_id.id,
-                    company_id=order.company_id.id)
-                if order.invoice_id:
-                    Invoice += order.invoice_id
-                    continue
+    # @api.multi
+    # def action_pos_order_invoice(self):
+    #     Invoice = self.env['account.invoice']
+    #     for order in self:
+    #         if not order.is_return_order:
+    #             super(PosOrder, order).action_pos_order_invoice()
+    #         else:
+    #             local_context = dict(
+    #                 self.env.context, force_company=order.company_id.id,
+    #                 company_id=order.company_id.id)
+    #             if order.invoice_id:
+    #                 Invoice += order.invoice_id
+    #                 continue
 
-                if not order.partner_id:
-                    raise UserError(
-                        _('Please provide a partner for the sale.'))
+    #             if not order.partner_id:
+    #                 raise UserError(
+    #                     _('Please provide a partner for the sale.'))
 
-                invoice = Invoice.new(order._prepare_invoice())
-                invoice._onchange_partner_id()
-                invoice.fiscal_position_id = order.fiscal_position_id
+    #             invoice = Invoice.new(order._prepare_invoice())
+    #             invoice._onchange_partner_id()
+    #             invoice.fiscal_position_id = order.fiscal_position_id
 
-                inv = invoice._convert_to_write(
-                    {name: invoice[name] for name in invoice._cache})
-                new_invoice = Invoice.with_context(
-                    local_context).sudo().create(inv)
-                message = _(
-                    "This invoice has been created from the point of sale session: <a href=# data-oe-model=pos.order data-oe-id=%d>%s</a>") % (order.id, order.name)
-                new_invoice.message_post(body=message)
-                order.write(
-                    {'invoice_id': new_invoice.id, 'state': 'invoiced'})
-                Invoice += new_invoice
+    #             inv = invoice._convert_to_write(
+    #                 {name: invoice[name] for name in invoice._cache})
+    #             new_invoice = Invoice.with_context(
+    #                 local_context).sudo().create(inv)
+    #             message = _(
+    #                 "This invoice has been created from the point of sale session: <a href=# data-oe-model=pos.order data-oe-id=%d>%s</a>") % (order.id, order.name)
+    #             new_invoice.message_post(body=message)
+    #             order.write(
+    #                 {'invoice_id': new_invoice.id, 'state': 'invoiced'})
+    #             Invoice += new_invoice
 
-                for line in order.lines:
-                    line.qty = abs(line.qty)
-                    self.with_context(local_context)._action_create_invoice_line(line, new_invoice.id)
-                    line.qty = -1 * line.qty
+    #             for line in order.lines:
+    #                 line.qty = abs(line.qty)
+    #                 self.with_context(local_context)._action_create_invoice_line(line, new_invoice.id)
+    #                 line.qty = -1 * line.qty
 
-                new_invoice.with_context(local_context).sudo().compute_taxes()
-                order.sudo().write({'state': 'invoiced'})
+    #             new_invoice.with_context(local_context).sudo().compute_taxes()
+    #             order.sudo().write({'state': 'invoiced'})
 
-            if not Invoice:
-                return {}
+    #         if not Invoice:
+    #             return {}
 
-            return {
-                'name': _('Customer Invoice'),
-                'view_type': 'form',
-                'view_mode': 'form',
-                'view_id': self.env.ref('account.invoice_form').id,
-                'res_model': 'account.invoice',
-                'context': "{'type':'out_refund'}",
-                'type': 'ir.actions.act_window',
-                'nodestroy': True,
-                'target': 'current',
-                'res_id': Invoice and Invoice.ids[0] or False,
-            }
+    #         return {
+    #             'name': _('Customer Invoice'),
+    #             'view_type': 'form',
+    #             'view_mode': 'form',
+    #             'view_id': self.env.ref('account.invoice_form').id,
+    #             'res_model': 'account.invoice',
+    #             'context': "{'type':'out_refund'}",
+    #             'type': 'ir.actions.act_window',
+    #             'nodestroy': True,
+    #             'target': 'current',
+    #             'res_id': Invoice and Invoice.ids[0] or False,
+    #         }
 
-    @api.model
-    def _order_fields(self, ui_order):
-        fields_return = super(PosOrder, self)._order_fields(ui_order)
-        fields_return.update({
-            'is_return_order': ui_order.get('is_return_order') or False,
-            'return_order_id': ui_order.get('return_order_id') or False,
-            'return_status': ui_order.get('return_status') or False,
-        })
-        return fields_return
+    # @api.model
+    # def _order_fields(self, ui_order):
+    #     fields_return = super(PosOrder, self)._order_fields(ui_order)
+    #     fields_return.update({
+    #         'is_return_order': ui_order.get('is_return_order') or False,
+    #         'return_order_id': ui_order.get('return_order_id') or False,
+    #         'return_status': ui_order.get('return_status') or False,
+    #     })
+    #     return fields_return
 
 
 class PosOrderLine(models.Model):
