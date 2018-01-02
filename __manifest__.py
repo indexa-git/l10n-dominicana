3f9088ba74596f2c3e622eeb71a6bcc0349c65e2
gustavovalverde <g.valverde02@gmail.com>
2018-01-02 08:40:16 -0400

    [IMP] Remove l10n_do dependency and use account_invoicing instead

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account', 'marcos_api_tools', 'l10n_do', 'account_cancel'],
+    'depends': ['account_invoicing', 'marcos_api_tools', 'account_cancel'],
     # always loaded
     'data': [
         'security/ir.model.access.csv',
