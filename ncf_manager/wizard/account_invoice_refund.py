3f48ad7583181308af407c775c1b54602d98edb0
gustavovalverde <g.valverde02@gmail.com>
2018-02-10 23:02:28 -0400

    [REF] Migrate NCF Manager to new public version

--- ncf_manager/wizard/account_invoice_refund.py
+++ ncf_manager/wizard/account_invoice_refund.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
-# Copyright 2004-2011 Pexego Sistemas Informáticos. (http://pexego.es)
-# Copyright 2014 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# ######################################################################
+# © 2015-2018 Marcos Organizador de Negocios SRL. (https://marcos.do/)
+#             Eneldo Serrata <eneldo@marcos.do>
+# © 2017-2018 iterativo SRL. (https://iterativo.do/)
+#             Gustavo Valverde <gustavo@iterativo.do>
+
+# This file is part of NCF Manager.
+
+# NCF Manager is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# NCF Manager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with NCF Manager.  If not, see <http://www.gnu.org/licenses/>.
+# ######################################################################
+
 import requests
 
 from odoo import models, api, fields, _
