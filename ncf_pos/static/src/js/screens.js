5737302ba087f0c32dc2753f720f65268ac63410
Jorge M. Hernandez S <dev.jhernandez@gmail.com>
2018-03-01 13:47:04 -0400

    add select event to the autocomplete, for fill the fields name and vat

--- ncf_pos/static/src/js/screens.js
+++ ncf_pos/static/src/js/screens.js
@@ -3,23 +3,31 @@ odoo.define('ncf_pos.screens', function (require) {
 
 
     var screens = require('point_of_sale.screens');
-   // var gui = require('point_of_sale.gui');
+    // var gui = require('point_of_sale.gui');
 
     screens.ClientListScreenWidget.include({
-        display_client_details: function (visibility,partner,clickpos) {
+        display_client_details: function (visibility, partner, clickpos) {
             var self = this;
 
-            this._super(visibility,partner,clickpos);
-            var name_input = this.$('.dgiiws');
+            this._super(visibility, partner, clickpos);
+            var name_input = this.$('input[name$=\'name\']');
+            var $rnc = $("input[name$='vat']");
 
-             
-                name_input.autocomplete({
-                    source: "/dgii_ws/",
-                    minLength: 3,
-                       
-                });
 
-        
+            name_input.autocomplete({
+                source: "/dgii_ws/",
+                minLength: 3,
+                select: function (event, ui) {
+                    
+                    name_input.val(ui.item.name);
+                    $rnc.val(ui.item.rnc);
+
+                    return false;
+                }
+
+            });
+
+
         }
 
     })
