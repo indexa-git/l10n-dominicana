44bbd64421f4313c7b2e7f1eec54728843454594
frankpenalo <rototen@gmail.com>
2018-03-19 13:34:46 -0400

    Show screen with order details (Ref: #67)

--- ncf_pos/static/src/js/screens.js
+++ ncf_pos/static/src/js/screens.js
@@ -31,12 +31,11 @@ odoo.define('ncf_pos.screens', function (require) {
     });
 
     /*--------------------------------------*\
-     |         THE INVOICES LIST              |
-     \*======================================*/
-
-    // The invoiceslist displays the list of invoices,
-    // and allows the cashier to reoder and rewrite the invoices.
-
+      THE INVOICES LIST
+     ======================================
+      The invoiceslist displays the list of invoices,
+      and allows the cashier to reoder and rewrite the invoices.
+     */
     var InvoicesListScreenWidget = screens.ScreenWidget.extend({
         template: 'InvoicesListScreenWidget',
 
@@ -63,7 +62,7 @@ odoo.define('ncf_pos.screens', function (require) {
             this.$('.invoices_search').on('keypress', function (event) {
                 if (event.which === 13)
                     self.perform_search(this.value);
-            });
+            }).focus();
 
             this.$('.searchbox .search-clear').click(function () {
                 self.clear_search();
@@ -90,11 +89,14 @@ odoo.define('ncf_pos.screens', function (require) {
         },
         render_list: function (orders) {
             var self = this;
-            var contents = this.$('.client-list-contents');
+            var contents = this.$('.order-list-contents');
 
             contents.empty();
-            $.each(orders, function (i, e) {
-                var rowHtml = QWeb.render('InvoicesLine', {widget: self, order: e});
+            this.pos.db.order_by_id = {};
+            orders.forEach(function (order) {
+                var rowHtml = QWeb.render('InvoicesLine', {widget: self, order: order});
+
+                self.pos.db.order_by_id[order.id] = order;
                 contents.append(rowHtml);
             });
         },
@@ -107,12 +109,105 @@ odoo.define('ncf_pos.screens', function (require) {
     });
     gui.define_screen({name: 'invoiceslist', widget: InvoicesListScreenWidget});
 
+    InvoicesListScreenWidget.include({
+        show: function () {
+            var self = this;
+            var contents = this.$('.order-details-contents');
+            var parent = this.$('.order_list').parent();
+
+            this._super();
+            contents.empty();
+            parent.scrollTop(0);
+            this.$('.order-list-contents').on('click', '.order-line', function (e) {
+                self.line_select(e, $(this), parseInt($(this).data('id')));
+            });
+        },
+        close: function () {
+            this._super();
+            this.$('.order-list-contents').off('click', '.order-line');
+        },
+        render_list: function (orders) {
+            this.display_order_details('hide');
+            this._super(orders);
+        },
+        line_select: function (event, $line, id) {
+            var self = this;
+            var order = self.pos.db.order_by_id[id];
+
+            this.$('.order_list .lowlight').removeClass('lowlight');
+
+            if ($line.hasClass('highlight')) {
+                this.display_order_details('hide');
+            }
+            else {
+                this.$('.order_list .highlight').removeClass('highlight');
+                $line.addClass('highlight');
+                self.selected_tr_element = $line;
+                var y = event.pageY - $line.parent().offset().top;
+                self.display_order_details('show', order, y);
+            }
+        },
+        display_order_details: function (visibility, order, clickpos) {
+            var self = this;
+            var contents = this.$('.order-details-contents');
+            var parent = this.$('.order_list').parent();
+            var scroll = parent.scrollTop();
+            var height = contents.height();
+            var new_height = 0;
+            var orderlines = order && order.lines;
+            var statements = [];
+
+            if (visibility === 'show') {
+                contents.empty();
+                contents.append($(QWeb.render('OrderDetails',
+                    {
+                        widget: this,
+                        order: order,
+                        orderlines: orderlines,
+                        statements: statements
+                    })));
+                new_height = contents.height();
+                if (!this.details_visible) {
+                    if (clickpos < scroll + new_height + 20) {
+                        parent.scrollTop(clickpos - 20);
+                    } else {
+                        parent.scrollTop(parent.scrollTop() + new_height);
+                    }
+                }
+                else {
+                    parent.scrollTop(parent.scrollTop() - height + new_height);
+                }
+                this.details_visible = true;
+
+                this.$("#close_order_details").on("click", function () {
+                    self.display_order_details('hide');
+                });
+            }
+            else if (visibility === 'hide') {
+                if (this.selected_tr_element) {
+                    this.selected_tr_element.removeClass('highlight');
+                    this.selected_tr_element.addClass('lowlight');
+                }
+                contents.empty();
+                if (height > scroll) {
+                    contents.css({height: height + 'px'});
+                    contents.animate({height: 0}, 400,
+                        function () {
+                            contents.css({height: ''});
+                        });
+                }
+                else {
+                    parent.scrollTop(parent.scrollTop() - height);
+                }
+                this.details_visible = false;
+            }
+        }
+    });
+
     screens.PaymentScreenWidget.include({
         show: function () {
             this._super();
             $(".button.js_invoice").remove();
         }
     });
-
-
 });
