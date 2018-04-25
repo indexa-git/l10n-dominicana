0feb2faf622daeb7d9723a70afd41f09277499e7
José López <joselopezg8@gmail.com>
2018-04-25 07:00:43 -0400

    [FIX] implement invoice NCF expiration date (#150)
    Close #140

--- ncf_manager/models/account_invoice.py
+++ ncf_manager/models/account_invoice.py
@@ -76,6 +76,14 @@ class AccountInvoice(models.Model):
             else:
                 inv.is_company_currency = False
 
+    @api.multi
+    @api.depends('state')
+    def get_ncf_expiration_date(self):
+        for inv in self:
+            if inv.state != 'draft':
+                inv.ncf_expiration_date = [dr.date_to for dr in inv.journal_id.date_range_ids if
+                                           dr.sale_fiscal_type == inv.sale_fiscal_type][0]
+
     # deprecate on odoo 11
     # def _default_user_shop(self):
     #     Shop = self.env["shop.ncf.config"]
