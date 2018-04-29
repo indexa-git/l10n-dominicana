8778ae9cc755bfc965d160fa51994c1bbac52699
Gustavo Valverde <gustavo@iterativo.do>
2018-04-29 01:51:20 -0400

    [FIX] Validate if there's a VAT before evaluating len

--- ncf_manager/models/account_invoice.py
+++ ncf_manager/models/account_invoice.py
@@ -329,11 +329,12 @@ class AccountInvoice(models.Model):
                         "para este tipo de factura.".format(inv.partner_id.id,
                                                             inv.partner_id.name)))
 
-                if inv.sale_fiscal_type == 'final' and len(inv.partner_id.vat) == 9:
-                    raise UserError(_(
-                        u"El cliente [{}]{} tiene RNC, no debe emitir una Factura"
-                        " de Consumo.".format(inv.partner_id.id,
-                                              inv.partner_id.name)))
+                if inv.sale_fiscal_type == 'final' and inv.partner_id.vat:
+                    if len(inv.partner_id.vat) == 9:
+                        raise UserError(_(
+                            u"No debe emitir una Factura de Consumo,"
+                            " a un cliente con RNC.".format(inv.partner_id.id,
+                                                            inv.partner_id.name)))
 
             elif inv.type in ("in_invoice", "in_refund"):
                 if inv.journal_id.purchase_type in ('normal', 'informal') and not inv.partner_id.vat:
