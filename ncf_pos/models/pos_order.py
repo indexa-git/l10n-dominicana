1e60f2723bc606ace3053b146f118a77504717b6
Eneldo Serrata <eneldoserrata@gmail.com>
2018-05-06 08:11:19 -0400

    [FIX] indica el NCF que afecta

--- ncf_pos/models/pos_order.py
+++ ncf_pos/models/pos_order.py
@@ -18,7 +18,7 @@ class PosOrder(models.Model):
             order.amount_paid += refund_payments
 
     is_return_order = fields.Boolean(string='Devolver Orden', copy=False)
-    return_order_id = fields.Many2one('pos.order', 'Devolver Orden de', readonly=True, copy=False)
+    return_order_id = fields.Many2one('pos.order', 'Afecta', readonly=True, copy=False)
     return_status = fields.Selection([('-', 'No Devuelta'), ('Fully-Returned', 'Totalmente Devuelta'),
                                       ('Partially-Returned', 'Parcialmente Devuelta'),
                                       ('Non-Returnable', 'No Retornable')], default='-', copy=False,
@@ -41,6 +41,8 @@ class PosOrder(models.Model):
                     'move_name': self.ncf,
                     'income_type': '01'
                 })
+            if self.return_order_id:
+                inv.update({'origin': self.return_order_id.invoice_id.number})
         return inv
 
     def test_paid(self):
