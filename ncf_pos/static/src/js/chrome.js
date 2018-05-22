7d12c53639677bf075611c9d40f3c2f7da5c83e0
Gustavo Valverde <g.valverde02@gmail.com>
2018-05-22 06:50:12 -0400

    [DEL] Remove SynchNotificationWidget from ncf_pos
    This was causing an odd behavior on Odoo and third party modules

--- ncf_pos/static/src/js/chrome.js
+++ ncf_pos/static/src/js/chrome.js
@@ -1,24 +1,6 @@
 odoo.define('ncf_pos.chrome', function (require) {
     "use strict";
     var chrome = require("point_of_sale.chrome");
-
-    /* ------- Synch. Notifications ------- */
-
-    // Displays if there are orders that could
-    // not be submitted, and how many.
-
-    chrome.SynchNotificationWidget = chrome.SynchNotificationWidget.include({
-        start: function(){
-            var self = this;
-            this.pos.bind('change:synch', function(pos,synch){
-                self.set_status(synch.state, synch.pending);
-            });
-            this.$el.click(function(){
-                self.pos.push_and_invoice_order(null,{'show_error':true});
-            });
-        },
-    });
-
     /**
      * Debug windows widget that appear in debug mode
      *
