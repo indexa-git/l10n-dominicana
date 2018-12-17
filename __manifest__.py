58e50a61de7bd73fa113bb8113f079323aaaa516
Jose Lopez <joselopezg8@gmail.com>
2018-12-17 01:17:44 -0400

    [REF] dgii_reports: account_accountant instead account

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'ncf_manager'],
+    'depends': ['base', 'account_accountant', 'ncf_manager'],
 
     # always loaded
     'data': [
