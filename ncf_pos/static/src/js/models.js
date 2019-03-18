d92b086a86a19249956de3f5f93403dca7684e4c
KolushovAlexandr <kolushov@it-projects.info>
2019-03-18 13:52:13 -0400

    :ambulance: different pos.config orders in the list

--- ncf_pos/static/src/js/models.js
+++ ncf_pos/static/src/js/models.js
@@ -43,7 +43,7 @@ odoo.define('ncf_pos.models', function (require) {
                 var validation_date = new Date(today.setDate(today.getDate() - self.config.number_of_days)).toISOString();
 
                 domain_list = [
-                    ['date_order', '>', validation_date],
+                    ['invoice_id.date_invoice', '>', validation_date],
                     ['state', 'not in', ['draft', 'cancel']],
                     ['config_id', '=', self.config.id]
                 ];
@@ -314,7 +314,7 @@ odoo.define('ncf_pos.models', function (require) {
             rpc.query({
                 model: 'pos.order',
                 method: 'order_search_from_ui',
-                args: [day_limit]
+                args: [day_limit, self.config.id]
             }, {})
                 .then(function (result) {
                     var orders = result && result.orders || [];
