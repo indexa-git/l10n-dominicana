fe08b97f3bd2e803e39babbd1380e793aa586323
Gustavo Valverde <gustavo@iterativo.do>
2019-03-19 13:21:05 -0400

    [FIX] ncf_pos: Add description to PosOrderNcfTemp

--- ncf_pos/models/pos_order.py
+++ ncf_pos/models/pos_order.py
@@ -267,6 +267,7 @@ class PosOrderLine(models.Model):
 
 class PosOrderNcfTemp(models.Model):
     _name = 'pos.order.ncf.temp'
+    _description = "NCF constraint for por orders"
 
     pos_reference = fields.Char(index=True)
     ncf = fields.Char("NCF")
