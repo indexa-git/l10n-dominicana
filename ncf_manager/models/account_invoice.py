c78be1834f946caa3260c7552a03bb53a64e1206
José López <joselopezg8@gmail.com>
2019-04-30 02:10:38 -0400

    [ADD] ncf_manager: added method to validate special expempt invoices (#373)
    * [ADD] ncf_manager: added method to validate special expempt invoices
    
    * [ADD] ncf_manager: made tests compliant with norma 05-19
    
    * [REF] ncf_manager: applied google yapf formatting
    
    * [FIX] ncf_manager: Remove linting errors
    
    * [FIX] ncf_manager: Oversight of previos commit

--- ncf_manager/models/account_invoice.py
+++ ncf_manager/models/account_invoice.py
@@ -82,10 +82,8 @@ class AccountInvoice(models.Model):
     purchase_type = fields.Selection(related="journal_id.purchase_type")
 
     sale_fiscal_type = fields.Selection(
-        [("final", "Consumo"),
-         ("fiscal", u"Crédito Fiscal"),
-         ("gov", "Gubernamentales"),
-         ("special", u"Regímenes Especiales"),
+        [("final", "Consumo"), ("fiscal", u"Crédito Fiscal"),
+         ("gov", "Gubernamentales"), ("special", u"Regímenes Especiales"),
          ("unico", u"Único Ingreso")],
         string='NCF para',
         default=lambda self: self._context.get('sale_fiscal_type', 'final'))
@@ -103,8 +101,7 @@ class AccountInvoice(models.Model):
     expense_type = fields.Selection(
         [('01', '01 - Gastos de Personal'),
          ('02', '02 - Gastos por Trabajo, Suministros y Servicios'),
-         ('03', '03 - Arrendamientos'),
-         ('04', '04 - Gastos de Activos Fijos'),
+         ('03', '03 - Arrendamientos'), ('04', '04 - Gastos de Activos Fijos'),
          ('05', u'05 - Gastos de Representación'),
          ('06', '06 - Otras Deducciones Admitidas'),
          ('07', '07 - Gastos Financieros'),
@@ -139,6 +136,30 @@ class AccountInvoice(models.Model):
     ncf_expiration_date = fields.Date('Válido hasta',
                                       compute="_compute_ncf_expiration_date",
                                       store=True)
+
+    @api.multi
+    @api.constrains('state', 'tax_line_ids')
+    def validate_special_exempt(self):
+        """ Validates an invoice with Regímenes Especiales sale_fiscal_type
+            does not contain nor ITBIS or ISC.
+
+            See DGII Norma 05-19, Art 3 for further information.
+        """
+        for inv in self:
+            if inv.type == 'out_invoice' and inv.state in (
+                    'open', 'cancel') and inv.sale_fiscal_type == 'special':
+
+                # If any invoice tax in ITBIS or ISC
+                if any([
+                        tax for tax in inv.tax_line_ids.mapped('tax_id')
+                        .filtered(lambda tax: tax.tax_group_id.name in (
+                            'ITBIS', 'ISC') and tax.amount != 0)
+                ]):
+                    raise UserError(_(
+                        "No puede validar una factura para Regímen Especial "
+                        " con ITBIS/ISC.\n\n"
+                        "Consulte Norma General 05-19, Art. 3 de la DGII")
+                    )
 
     def validate_fiscal_purchase(self):
         NCF = self.reference if self.reference else None
@@ -175,12 +196,17 @@ class AccountInvoice(models.Model):
                 ('reference', '=', NCF),
                 ('state', 'in', ('draft', 'open', 'paid', 'cancel')),
                 ('type', 'in', ('in_invoice', 'in_refund'))
-            ]) if self.id else self.search_count([
-                ('partner_id', '=', self.partner_id.id),
-                ('company_id', '=',  self.company_id.id),
-                ('reference', '=', NCF),
-                ('state', 'in', ('draft', 'open', 'paid', 'cancel')),
-                ('type', 'in', ('in_invoice', 'in_refund'))])
+            ]) if self.id else self.search_count([('partner_id', '=',
+                                                   self.partner_id.id),
+                                                  ('company_id', '=',
+                                                   self.company_id.id),
+                                                  ('reference', '=', NCF),
+                                                  ('state', 'in',
+                                                   ('draft', 'open',
+                                                    'paid', 'cancel')),
+                                                  ('type',
+                                                   'in', ('in_invoice',
+                                                          'in_refund'))])
 
             if ncf_in_invoice:
                 raise ValidationError(_(
@@ -272,15 +298,15 @@ class AccountInvoice(models.Model):
                 if not inv.partner_id.sale_fiscal_type:
                     raise ValidationError(_(
                         u"El cliente [{}]{} no tiene Tipo de comprobante, y es"
-                        "requerido para este tipo de factura.")
-                        .format(inv.partner_id.id, inv.partner_id.name))
+                        "requerido para este tipo de factura.").format(
+                            inv.partner_id.id, inv.partner_id.name))
 
                 if inv.sale_fiscal_type in (
                         "fiscal", "gov", "special") and not inv.partner_id.vat:
                     raise UserError(_(
                         u"El cliente [{}]{} no tiene RNC/Céd, y es requerido"
-                        "para este tipo de factura.")
-                        .format(inv.partner_id.id, inv.partner_id.name))
+                        "para este tipo de factura.".format(
+                            inv.partner_id.id, inv.partner_id.name)))
 
                 if (inv.amount_untaxed_signed >= 250000 and
                         inv.sale_fiscal_type != 'unico' and
@@ -333,8 +359,8 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def invoice_validate(self):
-        """ After all invoice validation routine, consume a NCF sequence and write it
-            into reference field.
+        """ After all invoice validation routine, consume a NCF sequence and
+            write it into reference field.
          """
         if not self.reference and (
                 self.journal_id.ncf_control or
