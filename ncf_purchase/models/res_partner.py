b9a49a1f698f4f728c5b6186afcffa1839f7f451
gustavovalverde <g.valverde02@gmail.com>
2019-05-02 14:49:25 -0400

    [FIX] ncf_purchase: Make the purchase journal multi-company

--- ncf_purchase/models/res_partner.py
+++ ncf_purchase/models/res_partner.py
@@ -21,4 +21,5 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     purchase_journal_id = fields.Many2one('account.journal',
+                                          company_dependent=True,
                                           domain=[('type', '=', 'purchase')])
