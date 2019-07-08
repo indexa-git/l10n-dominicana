e24e501261f3b4ba396b3b80dd197cd20b7459d4
Gustavo Valverde <g.valverde02@gmail.com>
2019-07-08 16:45:58 -0400

    [FIX] ncf_pos: Patch the payment assignment to invoices
    There was an error were an invoice was referenced to multiple credit notes

--- ncf_pos/models/pos_order.py
+++ ncf_pos/models/pos_order.py
@@ -154,7 +154,7 @@ class PosOrder(models.Model):
             if record.refund_payment_account_move_line_ids:
                 for aml in record.refund_payment_account_move_line_ids:
                     for p_id in aml.invoice_id.payment_move_line_ids.ids:
-                        record.invoice_id.assign_outstanding_credit(p_id)
+                        record.invoice_id[0].assign_outstanding_credit(p_id)
         return res
 
     @api.model
