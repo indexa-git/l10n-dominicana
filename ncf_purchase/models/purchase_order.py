cdc2159471bfbf69b2741197b0e7d0f5b8ae643d
Miguel Hiciano <mike29hiciano@gmail.com>
2019-08-10 11:10:15 -0400

    [FIX] ncf_purchase: fixing whitespaces

--- ncf_purchase/models/purchase_order.py
+++ ncf_purchase/models/purchase_order.py
@@ -26,11 +26,13 @@ class PurchaseOrder(models.Model):
         result = super(PurchaseOrder, self).action_view_invoice()
         supplier = self.partner_id
         result['context']['default_partner_id'] = supplier.id
-        
-        #remove default_reference from context when invoice
-        #is created from a purchase order
+
+        """
+        remove default_reference from context when invoice
+        is created from a purchase order
+        """
+
         del result['context']['default_reference']
-        
         if supplier.purchase_journal_id:
             result['context']['default_journal_id'] = \
                 supplier.purchase_journal_id.id
