8189200e68ff0d006ac2a0bcddc2390b730c6941
Jose Lopez <joselopezg8@gmail.com>
2019-10-22 00:48:26 -0400

    [REF] l10n_do_accounting: do not use length deprecated attrb name

--- l10n_do_accounting/models/account_invoice.py
+++ l10n_do_accounting/models/account_invoice.py
@@ -132,9 +132,9 @@ class AccountInvoice(models.Model):
                 fs_id = inv.fiscal_sequence_id  # Fiscal Sequence
                 remaining = fs_id.sequence_remaining
                 remaining_percent = fs_id.remaining_percentage
-                length = fs_id.sequence_end - fs_id.sequence_start + 1
+                seq_length = fs_id.sequence_end - fs_id.sequence_start + 1
 
-                consumed_percent = round(1 - (remaining / length), 2) * 100
+                consumed_percent = round(1 - (remaining / seq_length), 2) * 100
 
                 if consumed_percent < remaining_percent:
                     inv.fiscal_sequence_status = 'fiscal_ok'
