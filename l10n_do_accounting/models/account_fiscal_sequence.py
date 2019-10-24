1ec4f60ba6e3489df2529afb1c1aada9e7bf941b
Jose Lopez <joselopezg8@gmail.com>
2019-10-24 15:49:37 -0400

    [REF] l10n_do_accounting: flake8

--- l10n_do_accounting/models/account_fiscal_sequence.py
+++ l10n_do_accounting/models/account_fiscal_sequence.py
@@ -141,10 +141,9 @@ class AccountFiscalSequence(models.Model):
     def _compute_sequence_remaining(self):
         for rec in self:
             if rec.sequence_id:
-                rec.write(
-                    {'sequence_remaining':
-                         rec.sequence_end - rec.sequence_id.number_next_actual + 1
-                     })
+                sequence_remaining = rec.sequence_end - \
+                                     rec.sequence_id.number_next_actual + 1
+                rec.write({'sequence_remaining': sequence_remaining})
 
     @api.multi
     @api.depends('fiscal_type_id.prefix', 'sequence_id.padding',
