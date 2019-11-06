c0fb535db89a3b82f6ddb27cddb0d9ad27759354
Jose Lopez <joselopezg8@gmail.com>
2019-11-06 17:49:06 -0400

    [REF] l10n_do_accounting: fucking lint

--- l10n_do_accounting/tests/test_account_fiscal_sequence.py
+++ l10n_do_accounting/tests/test_account_fiscal_sequence.py
@@ -11,8 +11,13 @@
 # TODO: _validate_sequence_range() ValidationErrors raised
 # TODO: internal sequence is deleted when fiscal sequence is deleted
 # TODO: fiscal sequence is auto expired if expiration_date is today
-# TODO: when a draft fiscal sequence is confirmed, a internal sequence is created too with correct vals
-# TODO: when a draft fiscal sequence is confirmed, a new internal sequence is attached and state == 'active'
-# TODO: when a fiscal sequence is cancelled, its internal sequence is set to inactive and state == 'cancelled'
-# TODO: a fiscal sequence is auto-depleted when its get out of available sequences
-# TODO: a fiscal sequence of random type always returns the correct combination of prefix-padding-sequence string
+# TODO: when a draft fiscal sequence is confirmed, a internal sequence
+#  is created too with correct vals
+# TODO: when a draft fiscal sequence is confirmed, a new internal sequence
+#  is attached and state == 'active'
+# TODO: when a fiscal sequence is cancelled, its internal sequence is set to
+#  inactive and state == 'cancelled'
+# TODO: a fiscal sequence is auto-depleted when its get out of available
+#  sequences
+# TODO: a fiscal sequence of random type always returns the correct combination
+#  of prefix-padding-sequence string
