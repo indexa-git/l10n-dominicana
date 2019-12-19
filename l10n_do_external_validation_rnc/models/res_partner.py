106e383b8c4b34ad67a04f837be1e4eefe2cf9eb
Raul Ovalle <raulovallet@gmail.com>
2019-12-18 18:38:23 -0600

    [ADD] l10n_do_external_validation_rnc: res_parner can create wirte rnc on name

--- l10n_do_external_validation_rnc/models/res_partner.py
+++ l10n_do_external_validation_rnc/models/res_partner.py
@@ -41,25 +41,25 @@ class ResPartner(models.Model):
                 raise ValidationError(_(e))
 
             try:
-                #response:
-                # {
-                #     "status": "success",
-                #     "data": [
-                #         {
-                #             "sector": "LOS RESTAURADORES",
-                #             "street_number": "18",
-                #             "street": "4",
-                #             "economic_activity": "VENTA AL POR MAYOR DE SOFTWARE",
-                #             "phone": "9393231",
-                #             "tradename": "INDEXA",
-                #             "state": "ACTIVO",
-                #             "business_name": "INDEXA SRL",
-                #             "rnc": "131793916",
-                #             "payment_regime": "NORMAL",
-                #             "constitution_date": "2018-07-20"
-                #         }
-                #     ]
-                # }
+                    # response:
+                    # {
+                    #     "status": "success",
+                    #     "data": [
+                    #         {
+                    #             "sector": "LOS RESTAURADORES",
+                    #             "street_number": "18",
+                    #             "street": "4",
+                    #             "economic_activity": "VENTA DE SOFTWARE",
+                    #             "phone": "9393231",
+                    #             "tradename": "INDEXA",
+                    #             "state": "ACTIVO",
+                    #             "business_name": "INDEXA SRL",
+                    #             "rnc": "131793916",
+                    #             "payment_regime": "NORMAL",
+                    #             "constitution_date": "2018-07-20"
+                    #         }
+                    #     ]
+                    # }
                 return json.loads(response.text)
             except TypeError:
                 _logger.warning(_('No serializable data from API response'))
@@ -67,41 +67,37 @@ class ResPartner(models.Model):
 
     @api.model
     def create(self, vals):
-        if vals['vat']:
-            parner_search = self.search([('vat', '=', vals['vat'])], limit=1)
-            if not parner_search:
+        rnc = False
+        if vals['vat'] and vals['vat'].isdigit():
+            rnc = vals['vat']
+        elif vals['name'].isdigit():
+            rnc = vals['name']
+
+        if rnc:
+            partner_search = self.search([('vat', '=', rnc)], limit=1)
+            if not partner_search:
                 partner = self.env['res.partner'].\
-                    validate_rnc_cedula(vals.get('vat'))
+                    validate_rnc_cedula(rnc)
 
                 if partner and partner['data']:
                     vals['name'] = partner['data'][0]['business_name']
+                    vals['vat'] = rnc
                     if not vals['phone'] and partner['data'][0]['phone']:
                         vals['phone'] = partner['data'][0]['phone']
                     if not vals['street'] and partner['data'][0]['street']:
                         vals['street'] = partner['data'][0]['street'] + ', #' \
                                          + partner['data'][0]['street_number']\
                                          + ', ' + partner['data'][0]['sector']
-                    if len(vals['vat']) == 9:
+                    if len(rnc) == 9:
                         vals['is_company'] = True
                     else:
                         vals['is_company'] = False
 
                 else:
-                    raise UserError(_('RNC/Cedula %s not exist') % vals['vat'])
+                    raise UserError(_('RNC/Cedula %s not exist') % rnc)
             else:
                 raise UserError(_('RNC/Cedula %s exist with name %s')
-                                % (vals['vat'], parner_search.name))
-
-        # elif vals['name'].isdigit():
-        #     partner = self.env['res.partner'].validate_rnc_cedula(number=vals.get('name'), name='')
-        #     if not 'name' in partner:
-        #         raise ValidationError(u"Cédula no disponible en DGII")
-        #     vals['name'] = partner['name']
-        #     vals['vat'] = partner['vat']
-        #     vals['sale_fiscal_type'] = partner['sale_fiscal_type']
-        #     vals['company_type'] = partner['company_type']
-        #     if partner['is_company']:
-        #         vals['is_company'] = partner['is_company']
+                                % (rnc, partner_search.name))
 
         res = super(ResPartner, self).create(vals)
 
