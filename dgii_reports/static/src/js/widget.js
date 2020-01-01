13ae1e02b8d547913db420deb550ddb7bc70aa13
Raul Ovalle <raulovallet@gmail.com>
2019-12-31 19:50:20 -0500

    [FIX] Expected indentation of 16 spaces but found 12. [Error/indent]

--- dgii_reports/static/src/js/widget.js
+++ dgii_reports/static/src/js/widget.js
@@ -7,9 +7,9 @@ odoo.define('dgii_report.dgii_report_widget', function (require) {
     var UrlDgiiReportsWidget = UrlChar.extend({
         _renderReadonly: function () {
             this.$el.text(this.attrs.text || this.value)
-            .addClass('o_form_uri o_text_overflow')
-            .attr('target', '_blank')
-            .attr('href', "dgii_reports/"+this.value);
+                .addClass('o_form_uri o_text_overflow')
+                .attr('target', '_blank')
+                .attr('href', "dgii_reports/"+this.value);
         },
     });
 
