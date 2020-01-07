d33bd52fb9cdc156d94c58c2adead161091e52e6
Gustavo Valverde <g.valverde02@gmail.com>
2020-01-07 18:23:35 -0400

    [FIX] l10n_do_sale: Use correct method for l10n_do_accounting

--- l10n_do_sale/models/sale_order.py
+++ l10n_do_sale/models/sale_order.py
@@ -17,18 +17,30 @@ class SaleOrder(models.Model):
          This method may be overridden to implement custom invoice generation
          (making sure to call super() to establish a clean extension chain).
         """
-        # TODO: CreatE tests
+        # TODO: Create tests
         self.ensure_one()
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
         partner_id = self.partner_id
-        fiscal_type_id = partner_id.sale_fiscal_type_id
 
         if partner_id.parent_id and partner_id.parent_id.is_company:
-            invoice_vals[
-                'fiscal_type_id'] = partner_id.parent_id.sale_fiscal_type_id
-        elif not fiscal_type_id and partner_id.vat and partner_id.is_company:
-            invoice_vals['fiscal_type_id'] = 'fiscal'
+            parent_fiscal_type_id = partner_id.parent_id.sale_fiscal_type_id.id
+            invoice_vals['fiscal_type_id'] = parent_fiscal_type_id
+
+        elif partner_id.vat:
+            partner_fiscal_type_id = partner_id.sale_fiscal_type_id
+            if partner_fiscal_type_id:
+                invoice_vals['fiscal_type_id'] = partner_fiscal_type_id.id
+            if not partner_fiscal_type_id:
+                invoice_vals['fiscal_type_id'] = self.env[
+                    'account.fiscal.type'].search(
+                        [('type', '=', 'out_invoice'),
+                         ('prefix', '=', 'B01'),
+                         ], limit=1).id
         else:
-            invoice_vals['fiscal_type_id'] = fiscal_type_id
+            invoice_vals['fiscal_type_id'] = self.env[
+                'account.fiscal.type'].search(
+                    [('type', '=', 'out_invoice'),
+                     ('prefix', '=', 'B02'),
+                     ], limit=1).id
 
         return invoice_vals
