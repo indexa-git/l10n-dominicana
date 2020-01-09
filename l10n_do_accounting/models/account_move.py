900f74ea0d056921d244da83f7f18b5ef4d3c9eb
Gustavo Valverde <g.valverde02@gmail.com>
2020-01-09 09:52:30 -0400

    [REF] account_move: Simpler methods

--- l10n_do_accounting/models/account_move.py
+++ l10n_do_accounting/models/account_move.py
@@ -1,8 +1,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
-from dateutil.relativedelta import relativedelta
 import logging
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
 _logger = logging.getLogger(__name__)
 
 
@@ -10,63 +11,26 @@ class AccountMove(models.Model):
 
     _inherit = 'account.move'
 
-    @api.model
-    def _l10n_ar_get_document_number_parts(self, document_number, document_type_code):
-        # import shipments
-        if document_type_code in ['66', '67']:
-            pos = invoice_number = '0'
-        else:
-            pos, invoice_number = document_number.split('-')
-        return {'invoice_number': int(invoice_number), 'point_of_sale': int(pos)}
+    l10n_latam_internal_type = fields.Selection(
+        related='l10n_latam_document_type_id.internal_type'
+    )
 
-    l10n_do_internal_type = fields.Selection(related='l10n_latam_document_type_id.internal_type')
-
-    l10n_do_partner_type = fields.Selection(related='res_partner.l10n_do_dgii_tax_payer_type')
-
-    # Mostly used on reports
-    l10n_ar_afip_concept = fields.Selection(
-        compute='_compute_l10n_ar_afip_concept', selection='_get_afip_invoice_concepts', string="AFIP Concept",
-        help="A concept is suggested regarding the type of the products on the invoice but it is allowed to force a"
-        " different type if required.")
-
-    def _get_afip_invoice_concepts(self):
-        """ Return the list of values of the selection field. """
-        return [('1', 'Products / Definitive export of goods'), ('2', 'Services'), ('3', 'Products and Services'),
-                ('4', '4-Other (export)')]
-
-    @api.depends('invoice_line_ids', 'invoice_line_ids.product_id', 'invoice_line_ids.product_id.type', 'journal_id')
-    def _compute_l10n_ar_afip_concept(self):
-        recs_afip = self.filtered(lambda x: x.company_id.country_id == self.env.ref('base.ar') and x.l10n_latam_use_documents)
-        for rec in recs_afip:
-            rec.l10n_ar_afip_concept = rec._get_concept()
-        remaining = self - recs_afip
-        remaining.l10n_ar_afip_concept = ''
-
-    def _get_concept(self):
-        """ Method to get the concept of the invoice considering the type of the products on the invoice """
-        self.ensure_one()
-        invoice_lines = self.invoice_line_ids.filtered(lambda x: not x.display_type)
-        product_types = set([x.product_id.type for x in invoice_lines if x.product_id])
-        consumable = set(['consu', 'product'])
-        service = set(['service'])
-        mixed = set(['consu', 'service', 'product'])
-        # on expo invoice you can mix services and products
-        expo_invoice = self.l10n_latam_document_type_id.code in ['19', '20', '21']
-
-        # Default value "product"
-        afip_concept = '1'
-        if product_types == service:
-            afip_concept = '2'
-        elif product_types - consumable and product_types - service and not expo_invoice:
-            afip_concept = '3'
-        return afip_concept
+    l10n_do_partner_type = fields.Selection(
+        related='res_partner.l10n_do_dgii_tax_payer_type'
+    )
 
     def _get_l10n_latam_documents_domain(self):
         self.ensure_one()
         domain = super()._get_l10n_latam_documents_domain()
-        if self.journal_id.company_id.country_id == self.env.ref('base.ar'):
-            letters = self.journal_id._get_journal_letter(counterpart_partner=self.partner_id.commercial_partner_id)
-            domain += ['|', ('l10n_ar_letter', '=', False), ('l10n_ar_letter', 'in', letters)]
+        if self.journal_id.company_id.country_id == self.env.ref('base.do'):
+            letters = self.journal_id._get_journal_letter(
+                counterpart_partner=self.partner_id.commercial_partner_id
+            )
+            domain += [
+                '|',
+                ('l10n_do_letter', '=', False),
+                ('l10n_do_letter', 'in', letters),
+            ]
             codes = self.journal_id._get_journal_codes()
             if codes:
                 domain.append(('code', 'in', codes))
@@ -75,57 +39,34 @@ class AccountMove(models.Model):
     def _get_document_type_sequence(self):
         """ Return the match sequences for the given journal and invoice """
         self.ensure_one()
-        if self.journal_id.l10n_latam_use_documents and self.l10n_latam_country_code == 'AR':
-            if self.journal_id.l10n_ar_share_sequences:
-                return self.journal_id.l10n_ar_sequence_ids.filtered(
-                    lambda x: x.l10n_ar_letter == self.l10n_latam_document_type_id.l10n_ar_letter)
-            res = self.journal_id.l10n_ar_sequence_ids.filtered(
+        if self.journal_id.l10n_latam_use_documents and self.l10n_latam_country_code == 'CL':
+            res = self.journal_id.l10n_cl_sequence_ids.filtered(
                 lambda x: x.l10n_latam_document_type_id == self.l10n_latam_document_type_id)
             return res
         return super()._get_document_type_sequence()
 
-    @api.onchange('partner_id')
-    def _onchange_partner_journal(self):
-        """ This method is used when the invoice is created from the sale or subscription """
-        expo_journals = ['FEERCEL', 'FEEWS', 'FEERCELP']
-        for rec in self.filtered(lambda x: x.company_id.country_id == self.env.ref('base.ar') and x.journal_id.type == 'sale'
-                                 and x.l10n_latam_use_documents and x.partner_id.l10n_ar_afip_responsibility_type_id):
-            res_code = rec.partner_id.l10n_ar_afip_responsibility_type_id.code
-            domain = [('company_id', '=', rec.company_id.id), ('l10n_latam_use_documents', '=', True), ('type', '=', 'sale')]
-            journal = self.env['account.journal']
-            if res_code in ['8', '9', '10'] and rec.journal_id.l10n_ar_afip_pos_system not in expo_journals:
-                # if partner is foregin and journal is not of expo, we try to change to expo journal
-                journal = journal.search(domain + [('l10n_ar_afip_pos_system', 'in', expo_journals)], limit=1)
-            elif res_code not in ['8', '9', '10'] and rec.journal_id.l10n_ar_afip_pos_system in expo_journals:
-                # if partner is NOT foregin and journal is for expo, we try to change to local journal
-                journal = journal.search(domain + [('l10n_ar_afip_pos_system', 'not in', expo_journals)], limit=1)
-            if journal:
-                rec.journal_id = journal.id
+    def _get_l10n_latam_documents_domain(self):
+        self.ensure_one()
+        domain = super()._get_l10n_latam_documents_domain()
+        if (self.journal_id.l10n_latam_use_documents and
+                self.journal_id.company_id.country_id == self.env.ref('base.cl')):
+            if self.type in ['in_invoice', 'in_refund']:
+                if self.partner_id.l10n_cl_sii_taxpayer_type == '2':
+                    domain += [('code', '=', '71')]
+                return domain
+            document_type_ids = self.journal_id.l10n_cl_sequence_ids.mapped('l10n_latam_document_type_id').ids
+            domain += [('id', 'in', document_type_ids)]
+            if self.partner_id.l10n_cl_sii_taxpayer_type == '3':
+                domain += [('code', 'in', ['35', '38', '39', '41', '56', '61'])]
+        return domain
 
-    def post(self):
-        ar_invoices = self.filtered(lambda x: x.company_id.country_id == self.env.ref('base.ar') and x.l10n_latam_use_documents)
-        for rec in ar_invoices:
-            rec.l10n_ar_afip_responsibility_type_id = rec.commercial_partner_id.l10n_ar_afip_responsibility_type_id.id
-            if rec.company_id.currency_id == rec.currency_id:
-                l10n_ar_currency_rate = 1.0
-            else:
-                l10n_ar_currency_rate = rec.currency_id._convert(
-                    1.0, rec.company_id.currency_id, rec.company_id, rec.invoice_date or fields.Date.today(), round=False)
-            rec.l10n_ar_currency_rate = l10n_ar_currency_rate
-
-        # We make validations here and not with a constraint because we want validation before sending electronic
-        # data on l10n_ar_edi
-        ar_invoices._check_argentinian_invoice_taxes()
-        res = super().post()
-        self._set_afip_service_dates()
-        return res
-
-    def _reverse_moves(self, default_values_list=None, cancel=False):
-        if not default_values_list:
-            default_values_list = [{} for move in self]
-        for move, default_values in zip(self, default_values_list):
-            default_values.update({
-                'l10n_ar_afip_service_start': move.l10n_ar_afip_service_start,
-                'l10n_ar_afip_service_end': move.l10n_ar_afip_service_end,
-            })
-        return super()._reverse_moves(default_values_list=default_values_list, cancel=cancel)
+    @api.constrains('type', 'l10n_latam_document_type_id')
+    def _check_invoice_type_document_type(self):
+        super()._check_invoice_type_document_type()
+        for rec in self.filtered(lambda r: r.company_id.country_id == self.env.ref(
+                'base.cl') and r.l10n_latam_document_type_id):
+            tax_payer_type = rec.partner_id.l10n_cl_sii_taxpayer_type
+            latam_document_type_code = rec.l10n_latam_document_type_id.code
+            if not tax_payer_type and latam_document_type_code not in ['35', '38', '39', '41']:
+                raise ValidationError(_('Tax payer type is mandatory for this type of document. '
+                                        'Please set the current tax payer type of this client'))
