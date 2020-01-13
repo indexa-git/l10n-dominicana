388fc13e34f1bdcc1e84648acd4081538af70a41
Gustavo Valverde <g.valverde02@gmail.com>
2020-01-12 21:16:34 -0400

    [FIX] l10n_do_accounting: Do not add prefix to sequence and use l10n_latam_document_number

--- l10n_do_accounting/models/account_move.py
+++ l10n_do_accounting/models/account_move.py
@@ -28,6 +28,8 @@ class AccountMove(models.Model):
         selection='_get_l10n_do_annulment_type', string='Annulment Type', copy=False,
     )
 
+    l10n_latam_document_number = fields.Char(store=True)
+
     def _is_debit_note(self):
         self.ensure_one()
         if (
@@ -37,6 +39,32 @@ class AccountMove(models.Model):
             and self.ref
         ):
             return True if self.ref[-10:-8] == '03' else False
+
+    @api.depends('name')
+    def _compute_l10n_latam_document_number(self):
+        recs_with_name = self.filtered(lambda x: x.name != '/')
+        for rec in recs_with_name:
+            name = rec.name
+            doc_code_prefix = rec.l10n_latam_document_type_id.doc_code_prefix
+            if doc_code_prefix and name:
+                name = name.split(" ", 1)[-1]
+            rec.l10n_latam_document_number = rec.l10n_latam_document_number
+        remaining = self - recs_with_name
+        remaining.l10n_latam_document_number = False
+
+    @api.onchange('l10n_latam_document_type_id', 'l10n_latam_document_number')
+    def _inverse_l10n_latam_document_number(self):
+        moves = self.filtered(lambda m: m.l10n_latam_country_code != 'DO')
+
+        for rec in moves.filtered('l10n_latam_document_type_id'):
+            if not rec.l10n_latam_document_number:
+                rec.name = '/'
+            else:
+                l10n_latam_document_number = rec.l10n_latam_document_type_id._format_document_number(rec.l10n_latam_document_number)
+                if rec.l10n_latam_document_number != l10n_latam_document_number:
+                    rec.l10n_latam_document_number = l10n_latam_document_number
+
+        super(AccountMove, moves)._inverse_l10n_latam_document_number()
 
     def _get_l10n_latam_documents_domain(self):
         self.ensure_one()
