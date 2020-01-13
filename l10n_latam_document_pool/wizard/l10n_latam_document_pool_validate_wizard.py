83b8fa0b277104831b84052fff0643b7765a3c66
Gustavo Valverde <g.valverde02@gmail.com>
2020-01-13 14:06:49 -0400

    [FIX] l10n_latam_document_pool: Use correct manifest information

--- l10n_latam_document_pool/wizard/l10n_latam_document_pool_validate_wizard.py
+++ l10n_latam_document_pool/wizard/l10n_latam_document_pool_validate_wizard.py
@@ -1,6 +1,6 @@
 # TODO: poner authorship en todos los archivos .py (xml tamb?)
 
-from odoo import models, fields, api, _
+from odoo import models, fields, _
 from odoo.exceptions import ValidationError
 
 
@@ -9,13 +9,13 @@ class L10nLatamDocumentPoolValidateWizard(models.TransientModel):
     This Wizard purpose is to warn the user when attempt to change
     sequence state.
     """
+
     _name = 'l10n_latam.document.pool.validate_wizard'
     _description = 'Account Fiscal Sequence Validate Wizard'
 
     name = fields.Char()
     l10n_latam_sequence_id = fields.Many2one(
-        'l10n_latam.document.pool',
-        string='Fiscal sequence',
+        'l10n_latam.document.pool', string='Fiscal sequence',
     )
 
     def confirm_cancel(self):
@@ -28,4 +28,5 @@ class L10nLatamDocumentPoolValidateWizard(models.TransientModel):
                 self.l10n_latam_sequence_id._action_cancel()
         else:
             raise ValidationError(
-                _("There is no Fiscal Sequence to perform this action."))
+                _("There is no Fiscal Sequence to perform this action.")
+            )
