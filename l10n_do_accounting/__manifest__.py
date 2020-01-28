d69b1f14529d1501e9bb0f48d4e93775ead86787
Gustavo Valverde <g.valverde02@gmail.com>
2020-01-28 08:43:52 -0400

    [ADD] l10n_do_accounting: First invoice version

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -23,6 +23,7 @@
         'views/account_dgii_menuitem.xml',
         'views/account_journal_views.xml',
         'views/l10n_latam_document_type_views.xml',
+        'views/report_invoice.xml'
     ],
     'qweb': [
         # "static/src/xml/fiscal_sequence_warning_template.xml",
