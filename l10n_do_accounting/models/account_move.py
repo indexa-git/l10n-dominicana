fd235b4defd245ec9ff98914c8a7d124ea583205
Jose Lopez <joselopezg8@gmail.com>
2020-01-28 21:50:38 -0400

    [REF] l10n_do_accounting: fix vendor credit notes

--- l10n_do_accounting/models/account_move.py
+++ l10n_do_accounting/models/account_move.py
@@ -379,32 +379,10 @@ class AccountMove(models.Model):
                 if refund_type == "fixed_amount"
                 else self.amount_untaxed * (percentage / 100)
             )
-            res['line_ids'] = [
-                (0, 0,
-                 {'account_id': self.partner_id.property_account_receivable_id.id,
-                  'amount_currency': -0.0,
-                  'credit': price_unit,
-                  'debit': 0.0,
-                  'exclude_from_invoice_tab': True,
-                  'move_id': self.id,
-                  'partner_id': self.partner_id.id,
-                  'price_unit': price_unit*-1,
-                  'quantity': 1.0,
-                  'tax_exigible': True,
-                  'name': ' ',
-                  }),
-                (0, 0,
-                 {'account_id': account_id,
-                  'credit': 0.0,
-                  'debit': price_unit,
-                  'move_id': self.id,
-                  'name': reason or _("Refund"),
-                  'partner_id': self.partner_id.id,
-                  'price_unit': price_unit,
-                  'product_uom_id': False,
-                  'quantity': 1.0,
-                  'tax_base_amount': 0.0,
-                  'tax_exigible': True,
-                  })]
+            res['line_ids'] = False
+            res['invoice_line_ids'] = [(0, 0, {
+                'name': reason or _("Refund"),
+                'price_unit': price_unit,
+            })]
 
         return res
