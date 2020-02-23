7356de1f10fc7e0f80cdc6e45843357ae360ea4a
Jeffry Jesus De La Rosa <jeffryjesus@gmail.com>
2020-02-23 09:20:03 -0400

    Forgot Fix

--- l10n_do_accounting/wizard/account_invoice_refund.py
+++ l10n_do_accounting/wizard/account_invoice_refund.py
@@ -277,7 +277,8 @@ class AccountInvoiceRefund(models.TransientModel):
                 elif (
                     invoice.journal_id.l10n_do_ncf_remote_validation
                     and not ncf_validation.check_dgii(
-                    invoice.partner_id.vat, self.refund_reference)
+                        invoice.partner_id.vat, self.refund_reference
+                    )
                     ):
                     raise ValidationError(
                         _(
