2af8378b5fc383e1dcaa99b003fcfcc5ad08e0cb
Jose Lopez <joselopezg8@gmail.com>
2020-06-09 19:14:04 -0400

    [REF] l10n_do_accounting: remove all Debit Notes features

--- l10n_do_accounting/wizard/account_move_reversal.py
+++ l10n_do_accounting/wizard/account_move_reversal.py
@@ -11,25 +11,19 @@ class AccountMoveReversal(models.TransientModel):
             ("percentage", _("Percentage")),
             ("fixed_amount", _("Amount")),
         ]
-        if self._context.get("debit_note"):
-            selection.pop(0)
 
         return selection
 
     @api.model
     def _get_default_refund_type(self):
-        if self._context.get("debit_note"):
-            return "percentage"
         return "full_refund"
 
     @api.model
     def _get_refund_action_selection(self):
 
-        name = _("debit") if self._context.get("debit_note") else _("Refund")
-
         return [
-            ("draft_refund", _("Partial %s") % name),
-            ("apply_refund", _("Full %s") % name),
+            ("draft_refund", _("Partial Refund")),
+            ("apply_refund", _("Full Refund")),
         ]
 
     @api.model
@@ -56,12 +50,6 @@ class AccountMoveReversal(models.TransientModel):
     )
     percentage = fields.Float()
     amount = fields.Float()
-    account_id = fields.Many2one(
-        "account.account",
-        string="Account",
-        domain=[("deprecated", "=", False)],
-        default=_default_account,
-    )
 
     @api.model
     def default_get(self, fields):
@@ -99,82 +87,3 @@ class AccountMoveReversal(models.TransientModel):
                 reason=self.reason,
             ),
         ).reverse_moves()
-
-    def generate_debit_note_move(self):
-
-        moves = (
-            self.env['account.move'].browse(self.env.context['active_ids'])
-            if self.env.context.get('active_model') == 'account.move'
-            else self.move_id
-        )
-        invoice_type = self.env.context.get('debit_note')
-
-        # Create default values.
-        default_values_list = []
-        for move in moves:
-            price_unit = (
-                self.amount
-                if self.refund_type == "fixed_amount"
-                else move.amount_untaxed * (self.percentage / 100)
-            )
-            default_values_list.append(
-                {
-                    'date': self.date or move.date,
-                    'invoice_date': move.is_invoice(include_receipts=True)
-                    and (self.date or move.date)
-                    or False,
-                    'journal_id': self.journal_id
-                    and self.journal_id.id
-                    or move.journal_id.id,
-                    'invoice_payment_term_id': None,
-                    'auto_post': True
-                    if self.date > fields.Date.context_today(self)
-                    else False,
-                    'invoice_line_ids': [
-                        (
-                            0,
-                            0,
-                            {
-                                'name': self.reason,
-                                'price_unit': price_unit,
-                                'account_id': self.account_id.id,
-                            },
-                        )
-                    ],
-                    'is_debit_note': True,
-                    'type': invoice_type,
-                    'partner_id': move.partner_id.commercial_partner_id.id,
-                    'l10n_do_origin_ncf': move.l10n_latam_document_number,
-                    'l10n_do_income_type': move.l10n_do_income_type,
-                    'l10n_do_expense_type': move.l10n_do_expense_type,
-                    'l10n_latam_document_number': self.l10n_latam_document_number,
-                }
-            )
-        debit_moves = (
-            self.env['account.move']
-            .with_context(internal_type='debit_note')
-            .create(default_values_list)
-        )
-        if self.refund_action == 'apply_refund':
-            debit_moves.post()
-        action = {
-            'name': _('Debit Notes'),
-            'type': 'ir.actions.act_window',
-            'res_model': 'account.move',
-        }
-        if len(debit_moves) == 1:
-            action.update(
-                {'view_mode': 'form', 'res_id': debit_moves.id, }
-            )
-        else:
-            action.update(
-                {
-                    'view_mode': 'tree,form',
-                    'domain': [
-                        ('id', 'in', debit_moves.ids),
-                        ('type', '=', invoice_type),
-                        ('is_debit_note', '=', True),
-                    ],
-                }
-            )
-        return action
