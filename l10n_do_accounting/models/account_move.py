24e355ad5868d4efda05bc07c821fc4056b1a792
Jose Lopez <joselopezg8@gmail.com>
2020-06-30 21:27:11 -0400

    [IMP] l10n_do_accounting: change default ecf service environment

--- l10n_do_accounting/models/account_move.py
+++ l10n_do_accounting/models/account_move.py
@@ -140,7 +140,7 @@ class AccountMove(models.Model):
                 and i.l10n_do_ecf_sign_date
         ):
 
-            ecf_service_env = self.env.context.get("l10n_do_ecf_service_env", "eCF")
+            ecf_service_env = self.env.context.get("l10n_do_ecf_service_env", "TesteCF")
             doc_code_prefix = invoice.l10n_latam_document_type_id.doc_code_prefix
             has_sign_date = doc_code_prefix != "E32" or (
                         doc_code_prefix == "E32"
