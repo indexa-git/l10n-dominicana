08b9a74ed7161300bae967237ab6f7b0b5afccf7
Jose Lopez <joselopezg8@gmail.com>
2020-06-30 22:40:57 -0400

    [REF] l10n_do_accounting: flake8

--- l10n_do_accounting/models/l10n_latam_document_type.py
+++ l10n_do_accounting/models/l10n_latam_document_type.py
@@ -1,6 +1,7 @@
 import logging
 
-from odoo import models, fields, _
+# from odoo import models, fields, _
+from odoo import models, fields
 # from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
