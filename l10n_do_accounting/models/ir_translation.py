d30640dc01ce031d58070a102adebdfd4304a6ab
Raul Ovalle <raulovallet@gmail.com>
2020-07-30 00:19:27 -0400

    [update] update pos-addons repo

---
+++ l10n_do_accounting/models/ir_translation.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class IrTraslation(models.Model):
+    _inherit = 'ir.translation'
+
+    def init(self):
+        all_traslation_employee_payslips = self.search([
+            ('lang', '=', 'es_DO'),
+            ('src', '=', 'Credit Note'),
+        ])
+        for traslation_employee_payslip in all_traslation_employee_payslips:
+            traslation_employee_payslip.update({
+                'value': 'Nota de crédito',
+            })
+
+        # all_traslation_payslyp_batches = self.search([
+        #     ('lang', '=', 'es_DO'),
+        #     ('src', '=', 'Payslips Batches'),
+        # ])
+        # for traslation_payslip_batches in all_traslation_payslyp_batches:
+        #     traslation_payslip_batches.update({
+        #         'value': 'Nómina Colectiva',
+        #     })
+
+
