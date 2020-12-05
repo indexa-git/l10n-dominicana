6e314db0eaecf92772419a2d6f0f830732162acf
Jose Lopez <joselopezg8@gmail.com>
2020-12-04 20:36:46 -0400

    [REF] l10n_do_accounting: remove external validation shitty code

--- l10n_do_accounting/models/l10n_latam_document_type.py
+++ l10n_do_accounting/models/l10n_latam_document_type.py
@@ -1,23 +1,12 @@
-import logging
-
-# from odoo import models, fields, _
 from odoo import models, fields
-# from odoo.exceptions import UserError
-
-_logger = logging.getLogger(__name__)
-
-# try:
-#     from stdnum.do.ncf import is_valid
-# except (ImportError, IOError) as err:
-#     _logger.debug(err)
 
 
 class L10nLatamDocumentType(models.Model):
     _inherit = "l10n_latam.document.type"
 
     def _get_l10n_do_ncf_types(self):
-        """ Return a list of fiscal types and their respective sequence type to be used
-        on sequences, journals and document types. """
+        """Return a list of fiscal types and their respective sequence type to be used
+        on sequences, journals and document types."""
         return [
             ("fiscal", "01"),
             ("consumer", "02"),
@@ -58,7 +47,9 @@ class L10nLatamDocumentType(models.Model):
             ("in_debit_note", "Supplier Debit Note"),
         ]
     )
-    is_vat_required = fields.Boolean(default=False,)
+    is_vat_required = fields.Boolean(
+        default=False,
+    )
 
     def _get_document_sequence_vals(self, journal):
         """ Values to create the sequences """
@@ -78,9 +69,9 @@ class L10nLatamDocumentType(models.Model):
         return values
 
     def _format_document_number(self, document_number):
-        """ Make validation of Import Dispatch Number
-          * making validations on the document_number.
-          * format the document_number against a pattern and return it
+        """Make validation of Import Dispatch Number
+        * making validations on the document_number.
+        * format the document_number against a pattern and return it
         """
         self.ensure_one()
         if self.country_id != self.env.ref("base.do"):
@@ -89,16 +80,4 @@ class L10nLatamDocumentType(models.Model):
         if not document_number:
             return False
 
-        # msg = "'%s' " + _("is not a valid value for") + " '%s'.<br/>%s"
-
-        # Import NCF Number Validator
-        # if not is_valid(document_number):
-        #     raise UserError(
-        #         msg
-        #         % (
-        #             document_number,
-        #             self.name,
-        #             _('Please check the number and try again'),
-        #         )
-        #     )
         return document_number
