72e75b696156a7d9a625afed6f04c756d2718900
Jose Lopez <joselopezg8@gmail.com>
2020-12-15 11:18:24 -0400

    [REF] l10n_do_accounting: get country code from country_id field

--- l10n_do_accounting/wizard/account_move_reversal.py
+++ l10n_do_accounting/wizard/account_move_reversal.py
@@ -42,7 +42,7 @@ class AccountMoveReversal(models.TransientModel):
         return journal.default_debit_account_id.id
 
     l10n_latam_country_code = fields.Char(
-        related="move_id.company_id.l10n_do_country_code",
+        related="company_id.l10n_do_country_code",
         help="Technical field used to hide/show fields regarding the localization",
     )
     refund_type = fields.Selection(
