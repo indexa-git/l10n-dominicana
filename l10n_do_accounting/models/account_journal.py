88c08712cb519687f5acd0bb9a41f4ad86ef432d
Jose Lopez <joselopezg8@gmail.com>
2020-12-17 17:45:29 -0400

    [REF] l10n_do_accounting: remove code

--- l10n_do_accounting/models/account_journal.py
+++ l10n_do_accounting/models/account_journal.py
@@ -21,11 +21,6 @@ class AccountJournal(models.Model):
         selection="_get_l10n_do_payment_form",
         string="Payment Form",
     )
-    # l10n_do_sequence_ids = fields.One2many(
-    #     "ir.sequence",
-    #     "l10n_latam_journal_id",
-    #     string="Sequences",
-    # )
 
     def _get_all_ncf_types(self, types_list):
         """
@@ -114,49 +109,3 @@ class AccountJournal(models.Model):
         if self.type != "sale":
             return []
         return ["E"] if self.company_id.l10n_do_ecf_issuer else ["B"]
-
-    @api.model
-    def create(self, values):
-        """ Create Document sequences after create the journal """
-        res = super().create(values)
-        # res._l10n_do_create_document_sequences()
-        return res
-
-    def write(self, values):
-        """ Update Document sequences after update journal """
-        to_check = {"type", "l10n_latam_use_documents"}
-        res = super().write(values)
-        # if to_check.intersection(set(values.keys())):
-        #     for rec in self:
-        #         rec._l10n_do_create_document_sequences()
-        return res
-
-    def _l10n_do_create_document_sequences(self):
-        """IF DGII Configuration changes try to review if this can be done
-        and then create / update the document sequences"""
-        self.ensure_one()
-        if self.company_id.country_id != self.env.ref("base.do"):
-            return True
-        if not self.l10n_latam_use_documents:
-            return False
-
-        # sequences = self.l10n_do_sequence_ids
-        # sequences.unlink()
-
-        # Create Sequences
-        ncf_types = self._get_journal_ncf_types()
-        internal_types = ["invoice", "in_invoice", "debit_note", "credit_note"]
-        domain = [
-            ("country_id.code", "=", "DO"),
-            ("internal_type", "in", internal_types),
-            ("active", "=", True),
-            "|",
-            ("l10n_do_ncf_type", "=", False),
-            ("l10n_do_ncf_type", "in", ncf_types),
-        ]
-        documents = self.env["l10n_latam.document.type"].search(domain)
-        # for document in documents:
-        #     sequences |= self.env["ir.sequence"].create(
-        #         document._get_document_sequence_vals(self)
-        #     )
-        return self.env["ir.sequence"]
