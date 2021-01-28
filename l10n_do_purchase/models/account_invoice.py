28838d59cd2a8b35596b3ddb04b9a2341e38b776
Jose Lopez <joselopezg8@gmail.com>
2021-01-28 19:15:09 -0400

    [IMP] l10n_do_purchase: only consider l10n_do invoices

--- l10n_do_purchase/models/account_invoice.py
+++ l10n_do_purchase/models/account_invoice.py
@@ -6,11 +6,16 @@ from odoo import models, api
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @api.onchange("purchase_vendor_bill_id", "purchase_id")
+    @api.onchange("purchase_vendor_bill_id", "purchase_id", "company_id", "journal_id")
     def _onchange_purchase_auto_complete(self):
         # Compute ref.
         super()._onchange_purchase_auto_complete()
-        refs = set(self.line_ids.mapped("purchase_line_id.order_id.partner_ref"))
-        refs = [ref for ref in refs if ref]
-        self.narration = ",".join(refs)
-        self.ref = False
+
+        if (
+            self.company_id.country_id.code == "DO"
+            and self.l10n_latam_use_documents
+        ):
+            refs = set(self.line_ids.mapped("purchase_line_id.order_id.partner_ref"))
+            refs = [ref for ref in refs if ref]
+            self.narration = ",".join(refs)
+            self.ref = False
