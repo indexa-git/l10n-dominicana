8812af4391ca4d5b22451c22af62a6eddc632d86
Jose Lopez <joselopezg8@gmail.com>
2022-02-02 11:07:15 -0400

    [FIX] l10n_do_accounting: invoice template minor fixes pt.2 :')

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
-    "version": "14.0.1.7.6",
+    "version": "14.0.1.7.7",
     # any module necessary for this one to work correctly
     "depends": ["l10n_latam_invoice_document", "l10n_do"],
     # always loaded
