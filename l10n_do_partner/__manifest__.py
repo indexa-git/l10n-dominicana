3fff340cf2b5811ffd2b264217fa28edfeb51517
Jorge-C0 <jconcehernandez@gmail.com>
2022-03-21 14:49:03 -0400

    [IMP] l10n_do_accounting: do not allow duplicate contact with same vat
    Co-authored-by: AndrésR. <andres@iterativo.io>

---
+++ l10n_do_partner/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_do_partner",
+
+    'summary': """
+        Este módulo implementa la administración y gestión de los los 
+        contactos para el cumplimento de la norma 06-18 de la Dirección 
+        de Impuestos Internos en la República Dominicana.
+        """,
+
+    'author': "INDEXA ",
+    'website': "https://www.iterativo.io",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Localization',
+    'version': '13.0.0.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['l10n_do'],
+
+    # always loaded
+    # 'data': [
+    #     # 'security/ir.model.access.csv',
+    #     'views/views.xml',
+    #     'views/templates.xml',
+    # ],
+    # only loaded in demonstration mode
+#     'demo': [
+#         'demo/demo.xml',
+#     ],
+ }
