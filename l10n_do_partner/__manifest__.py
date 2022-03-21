529085e15536a48199a53c772eed2e58380d4954
Jorge-C0 <jconcehernandez@gmail.com>
2022-03-21 14:49:03 -0400

    [ADD] l10n_do_partner: do not allow duplicate contact with same vat

---
+++ l10n_do_partner/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "l10n_do_partner",
+    "summary": """
+        Este módulo implementa la administración y gestión de los
+        contactos para la República Dominicana.
+        """,
+    "author": "INDEXA",
+    "website": "https://www.iterativo.io",
+    "category": "Localization",
+    "version": "13.0.0.0.0",
+    "depends": ["l10n_do"],
+}
