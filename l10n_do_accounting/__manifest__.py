2a2f82c365c9768f69945d0e232f92d3aeeea746
Jose Lopez <joselopezg8@gmail.com>
2022-04-26 15:05:11 -0400

    [FIX] l10n_do_debit_note: use dynamic external id

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
-    "version": "14.0.2.7.13",
+    "version": "14.0.2.7.14",
     # any module necessary for this one to work correctly
     "depends": ["l10n_latam_invoice_document", "l10n_do"],
     # always loaded
