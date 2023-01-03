a24d7c7b7d4686aca71a0da09c5d7d1701e6aca1
José López <joselopezg8@gmail.com>
2023-01-03 18:32:16 -0400

    [FIX] l10n_do_accounting: wrong amount on qr stamp (#1020) (#1022)

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
-    "version": "15.0.0.7.0",
+    "version": "15.0.0.7.1",
     # any module necessary for this one to work correctly
     "depends": ["l10n_latam_invoice_document", "l10n_do"],
     # always loaded
