d5e10c4d028fd68e931e563f087033d5b6ed5349
Jose Lopez <joselopezg8@gmail.com>
2023-01-19 17:49:27 -0400

    [REF] l10n_do_accounting: change the way to get ECF env for QR stamp

--- l10n_do_accounting/tests/test_account_move.py
+++ l10n_do_accounting/tests/test_account_move.py
@@ -598,7 +598,7 @@ class AccountMoveTest(common.L10nDOTestsCommon):
         Check invoice electronic stamp feature works properly
         """
         stamp = (
-            "https%3A%2F%2Fecf.dgii.gov.do%2FCerteCF%2FConsultaTimbre%3FRncEmisor"
+            "https%3A%2F%2Fecf.dgii.gov.do%2FTesteCF%2FConsultaTimbre%3FRncEmisor"
             "%3D131793916%26RncComprador%3D131566332%26ENCF%3DE310000000001%26Fec"
             "haEmision%3D16-10-2021%26MontoTotal%3D118%26FechaFirma%3D16-10-2021%"
             "252000%3A00%3A00%26CodigoSeguridad%3Du83ac1"
