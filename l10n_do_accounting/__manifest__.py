fc3c98b810189df4e9a572bd6ed4411d2016c441
Daniel Alexander Pereyra Beltran <95594662+DanielAPereyraB@users.noreply.github.com>
2023-02-20 14:03:02 -0400

    [14.0][IMP] l10n_do_accounting: improvement in name_search (#1035)
    * [IMP]l10n_do_accounting: improvement in name_search
    
    * black solution
    
    * black solution

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
-    "version": "14.0.2.16.2",
+    "version": "14.0.2.16.3",
     # any module necessary for this one to work correctly
     "depends": ["l10n_latam_invoice_document", "l10n_do"],
     # always loaded
