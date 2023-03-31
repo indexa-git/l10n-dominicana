7ff3a818100851460645d9525a43f89adc4807b6
Jose Lopez <joselopezg8@gmail.com>
2023-03-31 11:25:18 -0400

    [IMP] l10n_do_accounting: improve memory usage when load l10n_latam.document.type.csv

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
-    "version": "14.0.2.16.6",
+    "version": "14.0.2.16.7",
     # any module necessary for this one to work correctly
     "depends": ["l10n_latam_invoice_document", "l10n_do"],
     # always loaded
--- l10n_do_accounting/models/account_move.py
+++ l10n_do_accounting/models/account_move.py
@@ -269,8 +269,8 @@ class AccountMove(models.Model):
         }
 
     @api.depends(
-        "company_id",
-        "l10n_latam_document_type_id.l10n_do_ncf_type",
+        "l10n_latam_country_code",
+        "l10n_latam_document_type_id",
     )
     def _compute_is_ecf_invoice(self):
         for invoice in self.filtered(lambda inv: inv.state == "draft"):
