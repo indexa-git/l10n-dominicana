9d88213ab5d8083d02b27b4e9dae8a96f0b1657b
Jose Lopez <joselopezg8@gmail.com>
2023-05-12 14:22:28 -0400

    [REF] l10n_do_accounting: ref test

--- l10n_do_accounting/tests/test_account_journal.py
+++ l10n_do_accounting/tests/test_account_journal.py
@@ -3,7 +3,7 @@ from odoo.tests import tagged
 from odoo.exceptions import RedirectWarning
 
 
-@tagged("post_install")
+@tagged("post_install_l10n", "-at_install", "post_install")
 class AccountJournalTest(common.L10nDOTestsCommon):
     def test_001_raise_redirect(self):
         """
