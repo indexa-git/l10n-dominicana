4234d520fc1f2748d50eddfd8973472dec833aa7
José López <joselopezg8@gmail.com>
2023-05-29 16:02:09 -0400

    [FIX] l10n_do_accounting: remove extra character form qr stamp url (#1058) (#1060)

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
-    "version": "16.0.0.0.1",
+    "version": "16.0.0.0.2",
     # any module necessary for this one to work correctly
     "depends": ["l10n_latam_invoice_document", "l10n_do"],
     # always loaded
