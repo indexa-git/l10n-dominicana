aa7e59e867a585e91ec5dad08c957be9f914541e
José López <joselopezg8@gmail.com>
2023-08-14 11:57:39 -0400

    [IMP] l10n_do_accounting: include ecf invoices on button_cancel validations

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
-    "version": "15.0.0.11.1",
+    "version": "15.0.0.11.2",
     # any module necessary for this one to work correctly
     "depends": ["l10n_latam_invoice_document", "l10n_do"],
     # always loaded
