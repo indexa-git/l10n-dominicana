0431370e22305f6cf3916f1c82bd7d4417c1b888
José López <joselopezg8@gmail.com>
2024-02-29 08:18:39 -0400

    [FIX] l10n_do_accounting: ValueError: day is out of range for month

--- l10n_do_accounting/models/ir_sequence.py
+++ l10n_do_accounting/models/ir_sequence.py
@@ -7,7 +7,8 @@ class IrSequence(models.Model):
     expiration_date = fields.Date(
         string="NCF Expiration date",
         default=fields.Date.end_of(
-            fields.Date.today().replace(year=fields.Date.today().year + 1), "year"
+            fields.Date.today().replace(month=12, year=fields.Date.today().year + 1),
+            "year",
         ),
     )
     # TODO: Note for anyone from the future: use l10n_do prefix on v14, for God sake
