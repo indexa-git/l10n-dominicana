d267e4a6ca82898559d558e3a8cd63c5ffc63819
Gleidy Espinal <1104225@est.intec.edu.do>
2024-06-25 11:45:50 -0400

    [FIX]l10n_do_accounting: removing maxdiff

--- l10n_do_accounting/tests/test_account_move.py
+++ l10n_do_accounting/tests/test_account_move.py
@@ -616,7 +616,6 @@ class AccountMoveTest(common.L10nDOTestsCommon):
             }
         )
         sale_invoice_1_id.with_context(l10n_do_active_test=True)._post()
-        self.maxDiff = None
         self.assertEqual(sale_invoice_1_id.l10n_do_electronic_stamp, stamp)
 
     def test_007_unique_sequence_number(self):
@@ -719,7 +718,6 @@ class AccountMoveTest(common.L10nDOTestsCommon):
                 "document_number": "B0100000001",
             }
         )
-        self.maxDiff = None
         self.assertDictEqual(
             invoice_1._get_l10n_do_amounts(),
             {
