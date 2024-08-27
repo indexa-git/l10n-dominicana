8692996a09b8c53256622a677ca1eeaa97f368c3
José López <joselopezg8@gmail.com>
2024-08-27 11:56:48 -0400

    [FIX] l10n_do_accounting: TypeError: AccountMove._name_search() got an unexpected keyword argument 'order'

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
-    "version": "17.0.1.0.2",
+    "version": "17.0.1.0.3",
     "countries": ["do"],
     # any module necessary for this one to work correctly
     "depends": ["l10n_latam_invoice_document", "l10n_do"],
