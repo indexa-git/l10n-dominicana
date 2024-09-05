5954e8b244095a326d82f44d0e5615b470a8604b
Erick Tejada <erick.tejada@tejadatech.com>
2024-09-05 12:34:40 -0400

    [IMP] moving the NCF fields to be outside of the partner DIV (labels were not properly shown on V17)

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "website": "https://github.com/odoo-dominicana",
-    "version": "17.0.1.0.3",
+    "version": "17.0.1.0.4",
     "countries": ["do"],
     # any module necessary for this one to work correctly
     "depends": ["l10n_latam_invoice_document", "l10n_do"],
