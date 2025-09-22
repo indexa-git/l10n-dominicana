11bdc3e9ae56bb89942e7c80732b764dd85efc01
DanielAPereyraB <pereyradaniel2020@gmail.com>
2025-09-22 13:40:50 -0400

    update depends

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -5,7 +5,7 @@
     "category": "Localization",
     "license": "LGPL-3",
     "version": "12.0.1.0.0",
-    "depends": ["l10n_latam_invoice_document", "l10n_do"],
+    "depends": ["l10n_do"],
     "installable": True,
     "auto_install": True,
     "post_init_hook": "post_init_hook",
