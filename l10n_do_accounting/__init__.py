0efece29b9d94e9e50a8ba86e33d85feac352d76
DanielAPereyraB <pereyradaniel2020@gmail.com>
2025-10-08 15:19:54 -0400

    Refactor l10n_do_accounting: update fiscal number assignment and remove l10n_do_ecf_invoicing module
    - Changed the assignment of l10n_do_fiscal_number in account_invoice to use reference instead of dgii_document_number.
    - Deleted the l10n_do_ecf_invoicing module, including its models and manifest, as it is no longer needed.

--- l10n_do_accounting/__init__.py
+++ l10n_do_accounting/__init__.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 def post_init_hook(cr, registry):
     cr.execute(
-        "UPDATE account_invoice SET l10n_do_fiscal_number = dgii_document_number;"
+        "UPDATE account_invoice SET l10n_do_fiscal_number = reference;"
     )
     cr.execute(
         "UPDATE account_journal SET l10n_latam_use_documents = use_documents;"
