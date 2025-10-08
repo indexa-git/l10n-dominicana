3ed601c085c881970974297f6e62c49d158434ab
DanielAPereyraB <pereyradaniel2020@gmail.com>
2025-10-08 15:36:30 -0400

    ncf_control

--- l10n_do_accounting/__init__.py
+++ l10n_do_accounting/__init__.py
@@ -10,5 +10,5 @@ def post_init_hook(cr, registry):
         "UPDATE account_invoice SET l10n_do_fiscal_number = reference;"
     )
     cr.execute(
-        "UPDATE account_journal SET l10n_latam_use_documents = use_documents;"
+        "UPDATE account_journal SET l10n_latam_use_documents = ncf_control;"
     )
