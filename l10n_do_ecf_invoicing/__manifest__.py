deb3527367dc151b83bec3b311e9bf36885b6eee
DanielAPereyraB <pereyradaniel2020@gmail.com>
2025-11-12 12:06:05 -0400

    ref(l10n_do_ecf_invoicing):update version

--- l10n_do_ecf_invoicing/__manifest__.py
+++ l10n_do_ecf_invoicing/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "iterativo LLC, " "Indexa",
     "category": "Localization",
     "license": "LGPL-3",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "depends": ["l10n_do_accounting"],
     "installable": True,
     "auto_install": True,
