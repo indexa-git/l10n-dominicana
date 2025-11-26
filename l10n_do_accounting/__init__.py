42f61378a2533d119c5e5a94d3ddfb7f66f8f8c5
DanielAPereyraB <pereyradaniel2020@gmail.com>
2025-11-26 11:25:37 -0400

    Add l10n_do_origin_ncf field to AccountInvoice model and update post-init hook to set its value from origin_out

--- l10n_do_accounting/__init__.py
+++ l10n_do_accounting/__init__.py
@@ -10,6 +10,9 @@ def post_init_hook(cr, registry):
         "UPDATE account_invoice SET l10n_do_fiscal_number = reference;"
     )
     cr.execute(
+        "UPDATE account_invoice SET l10n_do_origin_ncf = origin_out;"
+    )
+    cr.execute(
         "UPDATE account_journal SET l10n_latam_use_documents = TRUE WHERE type IN ('sale', 'purchase');"
     )
     cr.execute(
